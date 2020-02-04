9c19a0686a9c42187d7ee46fe7057a989d1f0e23
Hugo Hideki Yamashita <hugo.hideki@gmail.com>
2020-02-04 12:46:54 -0300

    Creating base project and setting up dependencies

---
+++ app/src/main/java/br/com/hugoyamashita/desafiobtg/ui/home/HomeViewModel.kt
@@ -0,0 +1,13 @@
+package br.com.hugoyamashita.desafiobtg.ui.home
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+class HomeViewModel : ViewModel() {
+
+    private val _text = MutableLiveData<String>().apply {
+        value = "This is home Fragment"
+    }
+    val text: LiveData<String> = _text
+}
