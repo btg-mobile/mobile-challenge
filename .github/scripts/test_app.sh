8933378ca2f2c82d9a941759e4d1e0ccea032e58
Vand <vandcarlosjunior@gmail.com>
2020-05-11 19:05:18 -0300

    Added xcpretty to github actions

--- .github/scripts/test_app.sh
+++ .github/scripts/test_app.sh
@@ -4,4 +4,4 @@ set -eo pipefail
 
 xcodebuild -scheme BTG\ Converser \
             -destination platform=iOS\ Simulator,OS=13.4.1,name=iPhone\ 11 \
-            clean test 
+            clean test | xcpretty
