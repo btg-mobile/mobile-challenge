dbc093d3eaab583cbd1f3e6839baa638510af14b
Vand <vandcarlosjunior@gmail.com>
2020-05-12 14:02:18 -0300

    Added UI tests

--- BTG ConverserUITests/BTG_ConverserUITests.swift
+++ BTG ConverserUITests/BTG_ConverserUITests.swift
@@ -11,33 +11,125 @@ import XCTest
 class BTG_ConverserUITests: XCTestCase {
 
     override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
-
-        // In UI tests it is usually best to stop immediately when a failure occurs.
         continueAfterFailure = false
-
-        // In UI tests it’s important to set the initial state - such as interface orientation - required for your tests before they run. The setUp method is a good place to do this.
     }
 
-    override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    func testSelectFromCode() throws {
+        let app = XCUIApplication()
+
+        app.launch()
+        app.buttons["from"].tap()
+
+        expectation(for: NSPredicate(format: "count > 0"), evaluatedWith: app.tables.cells, handler: nil)
+        waitForExpectations(timeout: 5, handler: nil)
+
+        let thirdCell = app.tables.firstMatch.cells.element(boundBy: 3)
+        let fromCode = thirdCell.staticTexts.firstMatch.label.prefix(3).description
+
+        thirdCell.tap()
+
+        let fromCodeLabel = app.staticTexts["From Code"]
+        let fromInput = app.textFields["From Input"]
+
+        expectation(for: NSPredicate(format: "exists == true"), evaluatedWith: fromCodeLabel, handler: nil)
+        expectation(for: NSPredicate(format: "label == %@", fromCode), evaluatedWith: fromCodeLabel, handler: nil)
+        expectation(for: NSPredicate(format: "isEnabled == true"), evaluatedWith: fromInput, handler: nil)
+
+        waitForExpectations(timeout: 3, handler: nil)
     }
 
-    func testExample() throws {
-        // UI tests must launch the application that they test.
+    func testSelectToCode() throws {
+        let app = XCUIApplication()
+
+        app.launch()
+        app.buttons["to"].tap()
+
+        expectation(for: NSPredicate(format: "count > 0"), evaluatedWith: app.tables.cells, handler: nil)
+        waitForExpectations(timeout: 5, handler: nil)
+
+        let fifth = app.tables.firstMatch.cells.element(boundBy: 5)
+        let toCode = fifth.staticTexts.firstMatch.label.prefix(3).description
+
+        fifth.tap()
+
+        let toLabel = app.staticTexts["Convert Value"]
+
+        expectation(for: NSPredicate(format: "exists == true"), evaluatedWith: toLabel, handler: nil)
+        expectation(for: NSPredicate(format: "label == %@", toCode), evaluatedWith: toLabel, handler: nil)
+
+        waitForExpectations(timeout: 3, handler: nil)
+    }
+
+    func testInputAcceptOnlyDecimalNumber() throws {
         let app = XCUIApplication()
         app.launch()
 
-        // Use recording to get started writing UI tests.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
+        app.buttons["from"].tap()
+
+        expectation(for: NSPredicate(format: "count > 0"), evaluatedWith: app.tables.cells, handler: nil)
+        waitForExpectations(timeout: 5, handler: nil)
+
+        app.tables.cells.element(boundBy: 3).tap()
+
+        app.buttons["to"].tap()
+
+        expectation(for: NSPredicate(format: "count > 0"), evaluatedWith: app.tables.cells, handler: nil)
+        waitForExpectations(timeout: 5, handler: nil)
+
+        app.tables.cells.element(boundBy: 5).tap()
+
+        let fromInput = app.textFields["From Input"]
+
+        fromInput.tap()
+        fromInput.typeText("foo")
+        fromInput.typeText("15.6")
+        fromInput.typeText("bar")
+        fromInput.typeText(".444")
+
+        let expectedValue = "15.6444" //The second dot is ignored
+
+        XCTAssertEqual(fromInput.value as! String, expectedValue)
     }
 
-    func testLaunchPerformance() throws {
-        if #available(macOS 10.15, iOS 13.0, tvOS 13.0, *) {
-            // This measures how long it takes to launch your application.
-            measure(metrics: [XCTOSSignpostMetric.applicationLaunch]) {
-                XCUIApplication().launch()
-            }
-        }
+    func testConvertValue() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        app.buttons["from"].tap()
+
+        expectation(for: NSPredicate(format: "count > 0"), evaluatedWith: app.tables.cells, handler: nil)
+        waitForExpectations(timeout: 5, handler: nil)
+
+        app.tables.cells.element(boundBy: 3).tap()
+
+        app.buttons["to"].tap()
+
+        expectation(for: NSPredicate(format: "count > 0"), evaluatedWith: app.tables.cells, handler: nil)
+        waitForExpectations(timeout: 5, handler: nil)
+
+        let fifthCurrency = app.tables.firstMatch.cells.element(boundBy: 5)
+        let toCode = fifthCurrency.staticTexts.firstMatch.label.prefix(3).description
+
+        fifthCurrency.tap()
+
+        let toLabel = app.staticTexts["Convert Value"]
+
+        expectation(for: NSPredicate(format: "exists == true"), evaluatedWith: toLabel, handler: nil)
+        expectation(for: NSPredicate(format: "label == %@", toCode), evaluatedWith: toLabel, handler: nil)
+        waitForExpectations(timeout: 3, handler: nil)
+
+        let fromInput = app.textFields["From Input"]
+
+        fromInput.tap()
+        fromInput.typeText("21.17")
+
+        app.buttons["CONVERT"].tap()
+
+        expectation(for: NSPredicate(format: "label != %@", toCode), evaluatedWith: toLabel, handler: nil)
+
+        waitForExpectations(timeout: 3, handler: nil)
+
+        XCTAssertTrue(toLabel.label.contains(toCode))
     }
+
 }
