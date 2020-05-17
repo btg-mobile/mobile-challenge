a0470c34d7401411f740fb0234b6b8a71ec2543a
leonardo maia pugliese <leo.pug@hotmail.com>
2020-05-16 22:20:40 -0300

    Cleaning Project and little layout changes

---
+++ BTGConverterAPP/BTGConverterAPP/Screens/Custom Views/Card/BTGConverterTitleCard.swift
@@ -0,0 +1,21 @@
+//
+//  BTGConverterTitleCard.swift
+//  BTGConverterAPP
+//
+//  Created by Ana Caroline de Souza on 16/05/20.
+//  Copyright © 2020 Leonardo Maia Pugliese. All rights reserved.
+//
+
+import UIKit
+
+class BTGConverterTitleCard: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
