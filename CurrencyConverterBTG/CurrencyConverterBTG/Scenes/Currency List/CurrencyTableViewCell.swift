ad63af42f236402071cbf8316d2c93898292659f
Silvia Florido <silvia.rodrigues@resourceit.com>
2020-06-19 15:36:11 -0300

    Basic layout

---
+++ CurrencyConverterBTG/CurrencyConverterBTG/Scenes/Currency List/CurrencyTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  CurrencyTableViewCell.swift
+//  CurrencyConverterBTG
+//
+//  Created by Silvia Florido on 19/06/20.
+//  Copyright © 2020 Silvia Florido. All rights reserved.
+//
+
+import UIKit
+
+class CurrencyTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
