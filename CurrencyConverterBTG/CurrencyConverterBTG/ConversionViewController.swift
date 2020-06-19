3dfc85865b804e36453a35553f73011bf7101197
smara <smara128@gmail.com>
2020-06-19 12:59:20 -0300

    Initial commit

---
+++ CurrencyConverterBTG/CurrencyConverterBTG/ConversionViewController.swift
@@ -0,0 +1,30 @@
+//
+//  ConversionViewController.swift
+//  CurrencyConverterBTG
+//
+//  Created by Silvia Florido on 18/06/20.
+//  Copyright © 2020 Silvia Florido. All rights reserved.
+//
+
+import UIKit
+
+class ConversionViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
