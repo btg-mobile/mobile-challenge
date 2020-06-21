896cd82e632d9b526d87ddba859fcc29ebcc77b4
Silvia Florido <silvia.rodrigues@resourceit.com>
2020-06-21 15:23:37 -0300

    Add CurrenciesList

---
+++ CurrencyConverterBTG/CurrencyConverterBTG/CurrenciesList.swift
@@ -0,0 +1,30 @@
+//
+//  CurrenciesList.swift
+//  CurrencyConverterBTG
+//
+//  Created by Silvia Florido on 21/06/20.
+//  Copyright © 2020 Silvia Florido. All rights reserved.
+//
+
+import Foundation
+
+class CurrenciesList: NSObject {
+    let currencies: [Currency]
+    
+    init(currencies: [Currency]) {
+        self.currencies = currencies
+    }
+    
+    convenience init(with response: CurrenciesListResponse) {
+        var currencies = response.currencies.map { (key, value) -> Currency in
+            return Currency(code: key, name: value)
+        }
+        currencies.sort{ $0.code.uppercased() < $1.code.uppercased() }
+        self.init(currencies: currencies)
+    }
+    
+    func getCurrency(fromCode: String) -> Currency? {
+        return currencies.first{ $0.code == fromCode }
+    }
+    
+}
