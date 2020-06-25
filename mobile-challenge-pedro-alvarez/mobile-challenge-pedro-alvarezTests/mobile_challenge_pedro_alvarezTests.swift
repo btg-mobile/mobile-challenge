d1059c1207468591ccc37ba7a3d90bbc2ff40782
pnalvarez <pedronalvarez@hotmail.com>
2020-06-24 21:15:25 -0300

    first commit - added gitignore

---
+++ mobile-challenge-pedro-alvarez/mobile-challenge-pedro-alvarezTests/mobile_challenge_pedro_alvarezTests.swift
@@ -0,0 +1,34 @@
+//
+//  mobile_challenge_pedro_alvarezTests.swift
+//  mobile-challenge-pedro-alvarezTests
+//
+//  Created by Pedro Alvarez on 24/06/20.
+//  Copyright © 2020 Pedro Alvarez. All rights reserved.
+//
+
+import XCTest
+@testable import mobile_challenge_pedro_alvarez
+
+class mobile_challenge_pedro_alvarezTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
