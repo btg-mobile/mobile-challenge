99d4a4a59c5ae543f1095892b8d6de1856f3db2f
Thiago Santos <>
2020-07-06 15:10:22 -0300

    [fix] Inserindo nova key

--- ConverterCurrencyBTG/ConverterCurrencyBTG/Commom/Manager/CurrencyRouter.swift
+++ ConverterCurrencyBTG/ConverterCurrencyBTG/Commom/Manager/CurrencyRouter.swift
@@ -23,10 +23,10 @@ enum CurrencyRouter: RequestProtocol {
     case live
     
     var accessKey: String {
-        return "f2954fa6f49f6af0b3fe2c631cc821a2"
+        return "2594e4fbc3f8c8609f91fc8c8abf63b1"
     }
 //f2954fa6f49f6af0b3fe2c631cc821a2
-//6e9a7ae3ac297c67a06197109c00ea2d
+//2594e4fbc3f8c8609f91fc8c8abf63b1
     
     func asURLRequest() throws -> URLRequest {
         
