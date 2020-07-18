2e6821a32ccecf7a8b4aeb0c202f24a642e60e6f
Ronilson Batista <ronilson.batista@orama.com.br>
2020-07-18 20:16:19 -0300

    including workspace

---
+++ CoinConversion/CoinConversion/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CoinConversion
+//
+//  Created by Ronilson Batista on 18/07/20.
+//  Copyright © 2020 Ronilson Batista. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ CoinConversion/Podfile
@@ -0,0 +1,12 @@
+# Uncomment the next line to define a global platform for your project
+# platform :ios, '9.0'
+
+target 'CoinConversion' do
+  # Comment the next line if you don't want to use dynamic frameworks
+  use_frameworks!
+
+  # Pods for CoinConversion
+
+  pod 'Alamofire', '~> 4.7'
+
+end
