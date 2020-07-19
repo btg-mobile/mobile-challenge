758c75e09a07549db6c934ad865429472efc9be5
Ronilson Batista <ronilson.batista@orama.com.br>
2020-07-18 22:36:22 -0300

    App router and mvvm base

---
+++ CoinConversion/CoinConversion/Scene/Conversion/ConversionRouter.swift
@@ -0,0 +1,23 @@
+//
+//  ConversionRouter.swift
+//  CoinConversion
+//
+//  Created by Ronilson Batista on 18/07/20.
+//  Copyright © 2020 Ronilson Batista. All rights reserved.
+//
+
+import UIKit
+
+// MARK: - Main
+class ConversionRouter {
+    
+    private let window: UIWindow
+    
+    init(window: UIWindow) {
+        self.window = window
+    }
+    
+    func createConversionScreen() {
+        print("dklldld")
+    }
+}
---
+++ CoinConversion/CoinConversion/Scene/Conversion/ConversionViewModel.swift
@@ -0,0 +1,9 @@
+//
+//  ConversionViewModel.swift
+//  CoinConversion
+//
+//  Created by Ronilson Batista on 18/07/20.
+//  Copyright © 2020 Ronilson Batista. All rights reserved.
+//
+
+import Foundation
