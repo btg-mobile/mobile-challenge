959ecead1623214e64b51539d7925cbb8f8fdc51
Raphael Martin <raphael.martin@newm.com.br>
2020-08-01 21:46:13 -0300

    Removendo libs comittadas incorretamente

--- BTGCurrency/Pods/Target Support Files/Pods-BTGCurrency/Pods-BTGCurrency-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_BTGCurrency : NSObject
-@end
-@implementation PodsDummy_Pods_BTGCurrency
-@end
--- BTGCurrency/Pods/Target Support Files/Pods-BTGCurrencyTests/Pods-BTGCurrencyTests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_BTGCurrencyTests : NSObject
-@end
-@implementation PodsDummy_Pods_BTGCurrencyTests
-@end
