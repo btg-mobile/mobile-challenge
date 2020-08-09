a236d37ec2e826e071950bb359a202ff8077c51e
Fernando Vieira <f02_@hotmail.com.br>
2020-08-09 01:53:53 -0300

    First commit

---
+++ CoinMaster/app/src/main/java/com/a/coinmaster/view/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.a.coinmaster.view
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.a.coinmaster.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
