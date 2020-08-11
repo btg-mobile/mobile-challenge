fa14c7e30c6f8f4ae344291c2ec7dfede2fc6d34
Henrique Hadermeck <henriquezanferrari@gmail.com>
2020-08-11 20:38:07 -0300

    Módulo presentation

---
+++ BTGChallenge/presentation/src/main/java/com/gft/presentation/App.kt
@@ -0,0 +1,15 @@
+package com.gft.presentation
+
+import android.app.Application
+
+class App : Application() {
+    override fun onCreate() {
+        super.onCreate()
+    }
+}
+
+// https://proandroiddev.com/demystifying-retrofit-network-call-with-kotlin-livedata-27437439137a
+// https://github.com/shasin89/weatherapp/blob/master/app/src/main/java/shasin/weatherapp/network/api/APIService.kt
+// http://api.currencylayer.com/list?access_key=502c1ba22d71723556c4142a6cd57156&format=1
+// https://www.raywenderlich.com/3595916-clean-architecture-tutorial-for-android-getting-started#toc-anchor-015
+// https://github.com/rakshit444/news-sample-app/blob/master/data/src/main/java/com/rakshitjain/data/repository/NewsRemoteImpl.kt
---
+++ BTGChallenge/presentation/src/main/java/com/gft/presentation/activity/ConvertActivity.kt
@@ -0,0 +1,13 @@
+package com.gft.presentation.activity
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.gft.presentation.R
+
+class ConvertActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_convert)
+    }
+}
