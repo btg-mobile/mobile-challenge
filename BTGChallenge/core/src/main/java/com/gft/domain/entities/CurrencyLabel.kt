e26c227df7c7f00cfbccffbfe04f2a0635d01a4c
Henrique Hadermeck <henriquezanferrari@gmail.com>
2020-08-10 22:22:51 -0300

    Criando Entidades

---
+++ BTGChallenge/core/src/main/java/com/gft/domain/entities/CurrencyLabel.kt
@@ -0,0 +1,6 @@
+package com.gft.domain.entities
+
+data class CurrencyLabel(
+    val codigo: String,
+    val nome: String
+)
---
+++ BTGChallenge/core/src/main/java/com/gft/domain/entities/CurrencyValue.kt
@@ -0,0 +1,6 @@
+package com.gft.domain.entities
+
+data class CurrencyValue(
+    val moedas: String,
+    val valor: Double
+)
