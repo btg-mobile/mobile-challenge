9a2d0e60747c58de2887964ac2d8aebe0d7b13d9
Henrique Hadermeck <henriquezanferrari@gmail.com>
2020-08-11 23:13:50 -0300

    Remoção do network info

--- BTGChallenge/data/src/main/java/com/gft/data/repository/CurrencyRepositoryImpl.kt
+++ BTGChallenge/data/src/main/java/com/gft/data/repository/CurrencyRepositoryImpl.kt
@@ -1,22 +1,17 @@
 package com.gft.data.repository
 
-import android.net.NetworkInfo
 import com.gft.data.datasource.CurrencyLocalDataSource
 import com.gft.data.datasource.CurrencyRemoteDataSource
 import com.gft.domain.entities.CurrencyLabelList
 import com.gft.domain.repository.CurrencyRepository
 import io.reactivex.Flowable
-import java.lang.Error
 
 class CurrencyRepositoryImpl(
     private val currencyLocalDataSource: CurrencyLocalDataSource,
-    private val currencyRemoteDataSource: CurrencyRemoteDataSource,
-    private val networkInfo: NetworkInfo
+    private val currencyRemoteDataSource: CurrencyRemoteDataSource
 ) : CurrencyRepository {
     override fun getLabels(): Flowable<CurrencyLabelList> {
-        if (networkInfo.isConnected)
-            return currencyRemoteDataSource.getLabels()
-        throw Error()
+        return currencyRemoteDataSource.getLabels()
     }
 
     override fun convert(from: String, to: String, value: Double): Double {
