4ed44579ddb4860dc4888bcd486bc7c1e12ae454
Eriberto Junior <eriberto.brasileiro@gmail.com>
2020-09-08 18:35:50 -0300

    ViewExtension.kt criado

---
+++ app/src/main/java/com/example/conversordemoeda/view/extensions/ViewExtension.kt
@@ -0,0 +1,11 @@
+package com.example.conversordemoeda.view.extensions
+
+import android.view.View
+
+fun View.show(){
+    visibility = View.VISIBLE
+}
+
+fun View.hide(){
+    visibility = View.GONE
+}
