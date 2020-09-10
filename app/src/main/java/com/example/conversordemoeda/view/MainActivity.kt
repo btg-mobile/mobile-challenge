bb3ce1eb0a3ebd18e7e6d0649703ed57050ee7f8
Eriberto Junior <eriberto.brasileiro@gmail.com>
2020-09-10 10:56:55 -0300

    Logica da MainActivity.kt e layout implementada

--- app/src/main/java/com/example/conversordemoeda/view/MainActivity.kt
+++ app/src/main/java/com/example/conversordemoeda/view/MainActivity.kt
@@ -2,12 +2,16 @@ package com.example.conversordemoeda.view
 
 import android.app.Activity
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.LifecycleObserver
+import androidx.lifecycle.Observer
 import com.example.conversordemoeda.R
+import com.example.conversordemoeda.model.repositorio.STATUS
 import com.example.conversordemoeda.view.ListaDeMoedasActivity.Companion.CODIGO_MOEDA_SELECIONADA
+import com.example.conversordemoeda.view.extensions.hide
+import com.example.conversordemoeda.view.extensions.onTextChanged
+import com.example.conversordemoeda.view.extensions.show
 import com.example.conversordemoeda.viewmodel.MainViewModel
 import kotlinx.android.synthetic.main.activity_main.*
 import org.koin.androidx.viewmodel.ext.android.viewModel
@@ -21,6 +25,10 @@ class MainActivity : AppCompatActivity(), LifecycleObserver {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        initObserver()
+
+        etValor.onTextChanged { configurarImputDeValor(it)}
+
         btCambioOrigem.setOnClickListener {
             irParaTelaDeCambio(CAMBIO_DE_ORIGEM)
         }
@@ -30,6 +38,34 @@ class MainActivity : AppCompatActivity(), LifecycleObserver {
         }
 
         lifecycle.addObserver(this)
+    }
+
+    private fun configurarImputDeValor(s: String) {
+        if (s.isEmpty()) {
+            viewmodel.setValorInformado(0F)
+            viewmodel.converterMoeda()
+        } else {
+            viewmodel.setValorInformado(s.toFloat())
+            viewmodel.converterMoeda()
+        }
+    }
+
+    private fun initObserver() {
+        viewmodel.cotacaoData.observe(this, Observer {
+            when (it.status) {
+                STATUS.OPEN_LOADING -> {
+                    progressBarMain.show()
+                }
+                STATUS.SUCCESS -> {
+                    progressBarMain.hide()
+                    tvValorConvertido.text = it.valorConvertido.toString()
+                }
+                STATUS.ERROR -> {
+                    progressBarMain.hide()
+                    tvValorConvertido.text = it.errorMessage
+                }
+            }
+        })
     }
 
     private fun irParaTelaDeCambio(tipodeCambio: Int) {
@@ -44,16 +80,21 @@ class MainActivity : AppCompatActivity(), LifecycleObserver {
                 when (requestCode) {
                     CAMBIO_DE_ORIGEM -> {
                         data?.let {
-                            Toast.makeText(this@MainActivity, it.getStringExtra(CODIGO_MOEDA_SELECIONADA), Toast.LENGTH_SHORT)
-                                .show()
-                            btCambioOrigem.text = it.getStringExtra(CODIGO_MOEDA_SELECIONADA)
+                            it.getStringExtra(CODIGO_MOEDA_SELECIONADA)?.let { cambioSelecionado ->
+                                btCambioOrigem.text = cambioSelecionado
+                                viewmodel.definirCambioDeOrigem(cambioSelecionado)
+                                viewmodel.converterMoeda()
+                            }
+
                         }
                     }
                     CAMBIO_DE_DESTINO -> {
                         data?.let {
-                            Toast.makeText(this@MainActivity, it.getStringExtra(CODIGO_MOEDA_SELECIONADA), Toast.LENGTH_SHORT)
-                                .show()
-                                btCambioDestino.text = it.getStringExtra(CODIGO_MOEDA_SELECIONADA)
+                            it.getStringExtra(CODIGO_MOEDA_SELECIONADA)?.let { cambioSelecionado ->
+                                btCambioDestino.text = cambioSelecionado
+                                viewmodel.definirCambioDeDestino(cambioSelecionado)
+                                viewmodel.converterMoeda()
+                            }
                         }
                     }
                 }
