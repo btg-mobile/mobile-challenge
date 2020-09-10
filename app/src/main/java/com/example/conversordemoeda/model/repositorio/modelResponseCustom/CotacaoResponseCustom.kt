ff2823ad61d3570da89fcb85a26e40918817df0a
Eriberto Junior <eriberto.brasileiro@gmail.com>
2020-09-10 10:54:54 -0300

    ajuste no CotacaoResponseCustom.kt

--- app/src/main/java/com/example/conversordemoeda/model/repositorio/modelResponseCustom/CotacaoResponseCustom.kt
+++ app/src/main/java/com/example/conversordemoeda/model/repositorio/modelResponseCustom/CotacaoResponseCustom.kt
@@ -1,11 +1,10 @@
 package com.example.conversordemoeda.model.repositorio.modelResponseCustom
 
-import com.example.conversordemoeda.model.entidades.Cotacao
 import com.example.conversordemoeda.model.repositorio.ResponseCustom
 import com.example.conversordemoeda.model.repositorio.STATUS
 
 class CotacaoResponseCustom(
-    var cotacao: Cotacao,
+    var valorConvertido: Float = 0F,
     errorMessage: String = "",
     status: STATUS
 ) : ResponseCustom(errorMessage, status)
