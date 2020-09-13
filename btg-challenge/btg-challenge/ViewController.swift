7db39d73a8185441e24917799f6ffb6e32d05df7
wesley.araujo <wesley.sickert@gmail.com>
2020-09-13 18:15:51 -0300

    start project

---
+++ btg-challenge/btg-challenge/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  btg-challenge
+//
+//  Created by Wesley Araujo on 13/09/20.
+//  Copyright © 2020 Wesley Araujo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
