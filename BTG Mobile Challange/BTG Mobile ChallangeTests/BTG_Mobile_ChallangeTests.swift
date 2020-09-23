4b113cc9bdbe42aa46b0a6a982312dbcd662ac54
Urielbp <urielbp@gmail.com>
2020-09-23 19:57:06 -0300

    Innitial commit

---
+++ BTG Mobile Challange/BTG Mobile ChallangeTests/BTG_Mobile_ChallangeTests.swift
@@ -0,0 +1,34 @@
+//
+//  BTG_Mobile_ChallangeTests.swift
+//  BTG Mobile ChallangeTests
+//
+//  Created by Uriel Barbosa Pinheiro on 23/09/20.
+//  Copyright © 2020 Uriel Barbosa Pinheiro. All rights reserved.
+//
+
+import XCTest
+@testable import BTG_Mobile_Challange
+
+class BTG_Mobile_ChallangeTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
