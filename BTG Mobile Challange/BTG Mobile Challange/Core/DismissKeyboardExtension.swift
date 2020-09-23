b804663653fdc9b27528ae0f24f436c917a6239c
Urielbp <urielbp@gmail.com>
2020-09-23 20:02:04 -0300

    Adding core components

---
+++ BTG Mobile Challange/BTG Mobile Challange/Core/DismissKeyboardExtension.swift
@@ -0,0 +1,24 @@
+//
+//  DismissKeyboardExtension.swift
+//  BTG Mobile Challange
+//
+//  Created by Uriel Barbosa Pinheiro on 23/09/20.
+//  Copyright © 2020 Uriel Barbosa Pinheiro. All rights reserved.
+//
+
+import UIKit
+
+extension UIViewController {
+    func setupToHideKeyboardOnTapOnView() {
+        let tap: UITapGestureRecognizer = UITapGestureRecognizer(
+            target: self,
+            action: #selector(UIViewController.dismissKeyboard))
+
+        tap.cancelsTouchesInView = false
+        view.addGestureRecognizer(tap)
+    }
+
+    @objc func dismissKeyboard() {
+        view.endEditing(true)
+    }
+}
