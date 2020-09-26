4666c909ff0d569120bbc1d6452c3bc598ffa118
Murilo Teixeira <murilogteixeira@gmail.com>
2020-09-26 00:05:01 -0300

    Add basic structure and navigation

--- mobile-challenge/mobile-challenge/View/ConverterViewController.swift
+++ mobile-challenge/mobile-challenge/View/ConverterViewController.swift
@@ -7,7 +7,7 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ConverterViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
---
+++ mobile-challenge/mobile-challenge/View/CurrencyListViewController.swift
@@ -0,0 +1,30 @@
+//
+//  CurrencyListViewController.swift
+//  mobile-challenge
+//
+//  Created by Murilo Teixeira on 25/09/20.
+//
+
+import UIKit
+
+class CurrencyListViewController: UIViewController {
+
+    @IBOutlet weak var navBar: UINavigationBar!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
