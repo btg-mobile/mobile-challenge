3fc79d5934194231b91c6ef8fc69c486e501ad6d
csfar <csf.artur@gmail.com>
2020-10-01 10:01:20 -0300

    Project setup

---
+++ btg-mobile-challenge/btg-mobile-challenge/ViewControllers/CurrencyConverterViewController.swift
@@ -0,0 +1,19 @@
+//
+//  CurrencyConverterViewController.swift
+//  btg-mobile-challenge
+//
+//  Created by Artur Carneiro on 01/10/20.
+//
+
+import UIKit
+
+final class CurrencyConverterViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .systemBlue
+    }
+
+
+}
+
