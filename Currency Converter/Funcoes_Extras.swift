8526b7f661dd8d8a1419ae8306d82756088072df
Gustavoy23 <gustavoy23@yahoo.com.br>
2020-10-01 21:48:23 -0300

    2nd commit

---
+++ Currency Converter/Funcoes_Extras.swift
@@ -0,0 +1,16 @@
+//
+//  Funcoes_Extras.swift
+//  Currency Converter
+//
+//  Created by Andreza on 10/1/20.
+//  Copyright © 2020 Gustavo Sousa. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+extension ViewController {
+    
+    
+    
+}
---
+++ Currency Converter/ViewController.swift
@@ -0,0 +1,271 @@
+//
+//  ViewController.swift
+//  Currency Converter
+//
+//  Created by Gustavo on 9/30/20.
+//  Copyright © 2020 Gustavo Sousa. All rights reserved.
+//
+
+
+// http://api.currencylayer.com/live?access_key=3e839fa4e1ee0756b657751b5a8b4277&format=1
+/* "live" endpoint - request the most recent exchange rate data
+
+ 
+ //3e839fa4e1ee0756b657751b5a8b4277
+ 
+ 
+http://api.currencylayer.com/live
+
+    ? access_key = YOUR_ACCESS_KEY
+    & source = GBP
+    & currencies = USD,AUD,CAD,PLN,MXN
+    & format = 1
+
+// Gustavo, click on the URL above to get the most recent exchange
+// rates for USD, AUD, CAD, PLN and MXN
+
+
+https://api.currencylayer.com/list
+    ? access_key = YOUR_ACCESS_KEY
+
+Example API Response:
+
+{
+    "success": true,
+    "terms": "https://currencylayer.com/terms",
+    "privacy": "https://currencylayer.com/privacy",
+    "currencies": {
+        "AED": "United Arab Emirates Dirham",
+        "AFN": "Afghan Afghani",
+        "ALL": "Albanian Lek",
+        "AMD": "Armenian Dram",
+        "ANG": "Netherlands Antillean Guilder",
+        [...]
+    }
+}
+ 
+ */
+
+let URL_conversion: String="http://api.currencylayer.com/live?"
+let URL_list: String="http://api.currencylayer.com/list?"
+let accesskey: String = "access_key=3e839fa4e1ee0756b657751b5a8b4277&format=1"
+
+import UIKit
+import Foundation
+
+class ViewController: UIViewController {
+    
+    //Objetos para Parse Json
+    struct cotacao: Codable {
+        var source:String?
+        var quotes:[String:Float]
+    }
+    
+    struct List: Codable {
+        var currencies: [String:String]
+    }
+    
+    //Variaveis da Tela
+    @IBOutlet var valor: UITextField!
+    @IBOutlet var fonte_button: UIButton!
+    @IBOutlet var destino_button: UIButton!
+    @IBOutlet var display_result: UILabel!
+    
+    //Variaveis do Picker List
+    @IBOutlet var picker: UIPickerView!
+    @IBOutlet var toolbar: UIImageView!
+    @IBOutlet var toolbar_display: UILabel!
+    @IBOutlet var ok_button: UIButton!
+   
+    //Dictionary e Arrays Globais Essenciais
+    var dict_lista:[String:String]?
+    var dict_quote:[String:Float]?
+    var arr_moeda:[String]?
+    
+    //Ao iniciar fazemos o download dos dados JSON
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        esconder()
+        convert_currency()
+        obtain_list()
+    }
+    
+    //Para inciar o mais logo as bordas do text e do label
+    override func viewDidAppear(_ animated: Bool) {
+        valor.layer.borderColor = UIColor.black.cgColor
+                      valor.layer.borderWidth=2.0
+          display_result.layer.borderColor = UIColor.black.cgColor
+            display_result.layer.borderWidth=2.0
+    }
+    
+    //
+    @IBAction func tecladoinicia(_ sender: Any) {
+       esconder()
+    }
+    
+    @IBAction func desapareceteclado(_ sender: Any) {
+        self.resignFirstResponder()
+    }
+    
+    func esconder(){
+        self.picker.isHidden=true
+        self.toolbar_display.isHidden=true
+        self.toolbar.isHidden=true
+        self.ok_button.isHidden=true
+    }
+    
+    func aparecer() {
+        self.picker.isHidden=false
+        self.toolbar_display.isHidden=false
+        self.toolbar.isHidden=false
+        self.ok_button.isHidden=false
+    }
+    
+    @IBAction func original(_ sender: Any) {
+        aparecer()
+        picker.reloadAllComponents()
+        self.toolbar_display.text="Moeda Fonte"
+    }
+    
+    @IBAction func result(_ sender: Any) {
+        aparecer()
+        picker.reloadAllComponents()
+        self.toolbar_display.text="Moeda Destino"
+    }
+    
+    @IBAction func OK(_ sender: Any) {
+        if(toolbar_display.text=="Moeda Fonte"){
+            if(fonte_button.currentTitle != "USD" && destino_button.currentTitle != "USD"){
+                destino_button.setTitle("USD", for: .normal)
+            }
+        } else {
+            if(destino_button.currentTitle != "USD" && fonte_button.currentTitle != "USD"){
+                   fonte_button.setTitle("USD", for: .normal)
+               }
+        }
+        
+        esconder()
+       // self.resignFirstResponder()
+ 
+    }
+    
+    @IBAction func calc(_ sender: Any) {
+        esconder()
+        self.calcular()
+    }
+    
+    func calcular(){
+        let fonte: String=self.fonte_button.currentTitle!
+        let destino:String=self.destino_button.currentTitle!
+        let input=Float(valor.text!)
+        var cotacao: Float
+        var tipo:String
+        
+        if(fonte_button.currentTitle=="USD"){
+            tipo="\(fonte)\(destino)"
+            cotacao=dict_quote![tipo]!*input!
+        } else {
+            tipo="\(destino)\(fonte)"
+            cotacao=input!/dict_quote![tipo]!
+            
+        }
+        display_result.text="\(cotacao)"
+  
+    }
+    
+    func convert_currency(){
+    
+    let URLString = URL_conversion + accesskey
+    let url=URL(string: URLString)
+    print(URLString)
+    guard url != nil else{
+        print("URL não existe")
+        return
+    }
+    
+    let session=URLSession.shared
+    let dataTask = session.dataTask(with:url!){ (data, response, error) in
+      if error == nil && data != nil {
+            let decoder = JSONDecoder()
+        do {
+            let tabela = try decoder.decode(cotacao.self, from: data!)
+            self.dict_quote = tabela.quotes
+            print("Teste:\n \(cotacao.self)")
+            print(self.dict_quote!)
+           }
+         catch {
+             print("Falha ao carregar arquivo JSON")
+        }
+        
+      }
+    }
+    
+    dataTask.resume()
+    }
+ 
+   // Metodo para obter List de Moedas, armazenando em Dictionaries e Arrays
+   func obtain_list(){
+   
+   let URLString = URL_list + accesskey
+   let url=URL(string: URLString)
+   
+    print(URLString)
+   guard url != nil else{
+       print("URL não existe")
+       return
+   }
+   
+   let session=URLSession.shared
+      let dataTask = session.dataTask(with:url!){ (data, response, error) in
+        if error == nil && data != nil {
+              let decoder = JSONDecoder()
+          do {
+              let tabela = try decoder.decode(List.self, from: data!)
+              self.dict_lista = tabela.currencies
+              self.arr_moeda = Array(self.dict_lista!.values)
+              print("Teste:\n \(List.self)")
+              print(self.dict_lista!)
+             }
+           catch {
+               print("Falha ao carregar arquivo JSON")
+          }
+        }
+      }
+      dataTask.resume()
+      }
+}
+
+
+
+//Metodos do PickerView
+extension ViewController: UIPickerViewDataSource, UIPickerViewDelegate {
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 1
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return dict_lista?.count ?? 20
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        return arr_moeda?[row]
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+        
+       // print(arr_moeda![row]!)
+        
+        let titulo:String=Array(dict_lista!.keys)[row]
+        
+        print(titulo)
+        if (toolbar_display.text=="Moeda Fonte"){
+            fonte_button.setTitle(titulo, for: .normal)
+        } else {
+            destino_button.setTitle(titulo, for: .normal)
+        }
+    }
+    
+    
+}
+
+
