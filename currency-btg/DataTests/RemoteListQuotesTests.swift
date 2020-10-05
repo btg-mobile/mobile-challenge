2316aaeeebf6fb12c61cedf566ca8ff4b250aad6
André Pimentel <andre.pimentel@iap.org.br>
2020-10-05 11:33:40 -0300

    refactor: move sut creation to a factory helper method

--- currency-btg/DataTests/RemoteListQuotesTests.swift
+++ currency-btg/DataTests/RemoteListQuotesTests.swift
@@ -10,20 +10,19 @@ class RemoteListQuotes {
     }
     
     func list() {
-        httpClient.get(url: url)
+        httpClient.get(to: url)
     }
 }
 
 protocol HttpGetClient {
-    func get(url: URL)
+    func get(to url: URL)
 }
 
 class RemoteListQuotesTests: XCTestCase {
 
     func test_list_should_call_httpClient_with_correct_url() throws {
         let url = URL(string: "http://any-url.com")!
-        let httpClientSpy = HttpClientSpy()
-        let sut = RemoteListQuotes(url: url, httpClient: httpClientSpy)
+        let (sut, httpClientSpy) = makeSut(url: url)
         
         sut.list()
         
@@ -32,10 +31,18 @@ class RemoteListQuotesTests: XCTestCase {
 }
 
 extension RemoteListQuotesTests {
+    func makeSut(url: URL = URL(string: "http://any-url.com")!) -> (sut: RemoteListQuotes, httpClientSpy: HttpClientSpy) {
+        
+        let httpClientSpy = HttpClientSpy()
+        let sut = RemoteListQuotes(url: url, httpClient: httpClientSpy)
+        
+        return (sut, httpClientSpy)
+    }
+    
     class HttpClientSpy: HttpGetClient {
         var url: URL?
         
-        func get(url: URL) {
+        func get(to url: URL) {
             self.url = url
         }
     }
