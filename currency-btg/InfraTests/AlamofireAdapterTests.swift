cf50a55dd523549d8845704a71b45cd1bc4ed82d
André Pimentel <andre.pimentel@iap.org.br>
2020-10-05 14:27:00 -0300

    chore: add Alamofire

--- currency-btg/InfraTests/AlamofireAdapterTests.swift
+++ currency-btg/InfraTests/AlamofireAdapterTests.swift
@@ -1,8 +1,15 @@
 import XCTest
 
+class AlamofireAdapter {
+    func get(to utl: URL) {
+        
+    }
+}
 class AlamofireAdapterTests: XCTestCase {
 
     func test_() throws {
-        
+        let url = makeUrl()
+        let sut = AlamofireAdapter()
+        sut.get(to: url)
     }
 }
