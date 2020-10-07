d864276ab7a45278e44a725e14a5f18294f64ece
André Pimentel <andre.pimentel@iap.org.br>
2020-10-07 14:43:51 -0300

    refactor: move sut creation to a factory helper method

--- currency-btg/UITests/RatesViewControllerTests.swift
+++ currency-btg/UITests/RatesViewControllerTests.swift
@@ -6,23 +6,26 @@ import Presenter
 class RatesViewControllerTests: XCTestCase {
 
     func test_loading_is_hidden_on_start() throws {
-        let sb = UIStoryboard(name: "Rates", bundle: Bundle(for: RatesViewController.self))
-        let sut = sb.instantiateViewController(identifier: "RatesViewController") as! RatesViewController
-        sut.loadViewIfNeeded()
+        let sut = makeSut()
         XCTAssertEqual(sut.loadingIndicator?.isAnimating, false)
     }
     
     func test_sut_implements_loadingView() throws {
-        let sb = UIStoryboard(name: "Rates", bundle: Bundle(for: RatesViewController.self))
-        let sut = sb.instantiateViewController(identifier: "RatesViewController") as! RatesViewController
-        sut.loadViewIfNeeded()
+        let sut = makeSut()
         XCTAssertNotNil(sut as LoadingView)
     }
     
     func test_sut_implements_alertView() throws {
+        let sut = makeSut()
+        XCTAssertNotNil(sut as AlertView)
+    }
+}
+
+extension RatesViewControllerTests {
+    func makeSut() -> RatesViewController {
         let sb = UIStoryboard(name: "Rates", bundle: Bundle(for: RatesViewController.self))
         let sut = sb.instantiateViewController(identifier: "RatesViewController") as! RatesViewController
         sut.loadViewIfNeeded()
-        XCTAssertNotNil(sut as AlertView)
+        return sut
     }
 }
