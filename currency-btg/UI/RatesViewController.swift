75cd56180e7b4cd8d0ac2de625179f0bc70f34bf
André Pimentel <andre.pimentel@iap.org.br>
2020-10-07 14:25:57 -0300

    test: ensure RatesViewController has a loading indicator and its hidden by default

---
+++ currency-btg/UI/RatesViewController.swift
@@ -0,0 +1,10 @@
+import Foundation
+import UIKit
+
+class RatesViewController: UIViewController {
+    @IBOutlet weak var loadingIndicator: UIActivityIndicatorView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
--- currency-btg/UITests/RatesViewControllerTests.swift
+++ currency-btg/UITests/RatesViewControllerTests.swift
@@ -1,8 +1,13 @@
 import XCTest
+import UIKit
+@testable import UI
 
 class RatesViewControllerTests: XCTestCase {
 
-    func test_() throws {
-        
+    func test_is_hidden_on_start() throws {
+        let sb = UIStoryboard(name: "Rates", bundle: Bundle(for: RatesViewController.self))
+        let sut = sb.instantiateViewController(identifier: "RatesViewController") as! RatesViewController
+        sut.loadViewIfNeeded()
+        XCTAssertEqual(sut.loadingIndicator?.isAnimating, false)
     }
 }
