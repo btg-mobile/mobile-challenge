0ba8530d18f9a51e0a31d81bba2936fc770fa6b5
André Pimentel <andre.pimentel@iap.org.br>
2020-10-07 18:43:18 -0300

    refactor: move MainQueueDipatchDecorator to its own file

--- currency-btg/Main/Factories/UseCaseFactory.swift
+++ currency-btg/Main/Factories/UseCaseFactory.swift
@@ -16,28 +16,3 @@ final class UseCaseFactory {
         return MainQueueDipatchDecorator(remoteListQuotes)
     }
 }
-
-public final class MainQueueDipatchDecorator<T> {
-    private let instance: T
-    
-    public init(_ instance: T) {
-        self.instance = instance
-    }
-    
-    func dispatch(completion: @escaping () -> Void) {
-        guard Thread.isMainThread else { return DispatchQueue.main.async(execute: completion) }
-        completion()
-        
-        
-    }
-}
-
-extension MainQueueDipatchDecorator: ListQuotes where T: ListQuotes {
-    public func list(completion: @escaping (Result<QuotesModel, DomainError>) -> Void) {
-        instance.list { [weak self] result in
-            self?.dispatch {
-                completion(result)
-            }
-        }
-    }
-}
