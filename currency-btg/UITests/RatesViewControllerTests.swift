2873eaeda12bade1aa0cccb340e02ee1f4e84d48
André Pimentel <andre.pimentel@iap.org.br>
2020-10-07 14:39:53 -0300

    test: ensure RatesViewController implements LoadingView

--- currency-btg/UITests/RatesViewControllerTests.swift
+++ currency-btg/UITests/RatesViewControllerTests.swift
@@ -1,13 +1,21 @@
 import XCTest
 import UIKit
+import Presenter
 @testable import UI
 
 class RatesViewControllerTests: XCTestCase {
 
-    func test_is_hidden_on_start() throws {
+    func test_loading_is_hidden_on_start() throws {
         let sb = UIStoryboard(name: "Rates", bundle: Bundle(for: RatesViewController.self))
         let sut = sb.instantiateViewController(identifier: "RatesViewController") as! RatesViewController
         sut.loadViewIfNeeded()
         XCTAssertEqual(sut.loadingIndicator?.isAnimating, false)
+    }
+    
+    func test_sut_implements_loadingView() throws {
+        let sb = UIStoryboard(name: "Rates", bundle: Bundle(for: RatesViewController.self))
+        let sut = sb.instantiateViewController(identifier: "RatesViewController") as! RatesViewController
+        sut.loadViewIfNeeded()
+        XCTAssertNotNil(sut as LoadingView)
     }
 }
