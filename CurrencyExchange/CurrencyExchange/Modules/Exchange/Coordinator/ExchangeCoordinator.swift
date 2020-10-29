2afd791887138aa4cebbae141501465361ffc1e5
Carlos <carloshenriquenlb@gmail.com>
2020-10-29 19:29:56 -0300

    Setup Exchange module

--- CurrencyExchange/CurrencyExchange/Modules/Exchange/Coordinator/ExchangeCoordinator.swift
+++ CurrencyExchange/CurrencyExchange/Modules/Exchange/Coordinator/ExchangeCoordinator.swift
@@ -12,17 +12,19 @@ final class ExchangeCoordinator: Coordinator {
     // MARK: - Properties
     
     var presenter: UINavigationController
+    private var exchangeViewController: ExchangeViewController
+    private var exchangeViewModel: ExchangeViewModel
     
     init(presenter: UINavigationController) {
         self.presenter = presenter
+        self.exchangeViewModel = ExchangeViewModel()
+        self.exchangeViewController = ExchangeViewController(viewModel: exchangeViewModel)
     }
     
     // MARK: - Methods
     
     func start() {
-        let controller = ExchangeViewController()
-        
-        self.presenter.pushViewController(controller, animated: true)
+        self.presenter.pushViewController(exchangeViewController, animated: true)
     }
     
     
---
+++ CurrencyExchange/CurrencyExchange/Modules/Exchange/ViewModel/ExchangeViewModel.swift
@@ -0,0 +1,17 @@
+//
+//  ExchangeViewModel.swift
+//  CurrencyExchange
+//
+//  Created by Carlos Fontes on 29/10/20.
+//
+
+import Foundation
+
+final class ExchangeViewModel {
+    
+    
+    init() {
+        
+    }
+    
+}
