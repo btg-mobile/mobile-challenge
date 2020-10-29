fe40a35fcd2c4220b100a6cceb0cee30c0dacd4b
Carlos <carloshenriquenlb@gmail.com>
2020-10-29 19:28:43 -0300

    Add ScreenSize

---
+++ CurrencyExchange/CurrencyExchange/Helpers/ScreenSize.swift
@@ -0,0 +1,19 @@
+//
+//  ScreenSize.swift
+//  CurrencyExchange
+//
+//  Created by Carlos Fontes on 29/10/20.
+//
+
+import UIKit
+
+struct ScreenSize {
+    static var width: CGFloat {
+        return UIScreen.main.bounds.width
+    }
+    
+    static var height: CGFloat {
+        return UIScreen.main.bounds.height
+    }
+}
+
