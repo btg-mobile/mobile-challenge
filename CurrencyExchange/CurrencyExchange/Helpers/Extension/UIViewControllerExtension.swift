3f674e79aab03f6107942351b637086afb51d4e3
Carlos <carloshenriquenlb@gmail.com>
2020-10-29 19:29:22 -0300

    Add UIViewController extension to setup navigation controller and show error

---
+++ CurrencyExchange/CurrencyExchange/Helpers/Extension/UIViewControllerExtension.swift
@@ -0,0 +1,40 @@
+//
+//  UIViewControllerExtension.swift
+//  CurrencyExchange
+//
+//  Created by Carlos Fontes on 29/10/20.
+//
+
+
+import UIKit
+
+extension UIViewController {
+        
+    func setupNavigationController(){
+        self.navigationController?.navigationBar.setBackgroundImage(UIImage(), for: .default)
+        self.navigationController?.navigationBar.shadowImage = UIImage()
+        
+    }
+    
+    
+    func showError(with message: String){
+        let alert = UIAlertController(
+            title: "Error",
+            message: message,
+            preferredStyle: .alert
+        )
+        
+        let okAction = UIAlertAction(
+            title: "Ok",
+            style: .default,
+            handler: nil
+        )
+        
+        alert.addAction(okAction)
+        
+        DispatchQueue.main.async {
+            self.present(alert, animated: true)
+        }
+    }
+    
+}
