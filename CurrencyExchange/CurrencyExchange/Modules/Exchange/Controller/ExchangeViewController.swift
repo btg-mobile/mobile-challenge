4adf836dbc318273306de5d65424518ab0899d1c
Carlos <carloshenriquenlb@gmail.com>
2020-11-01 19:49:49 -0300

    Add delegate to reload data when currency exchange end up

--- CurrencyExchange/CurrencyExchange/Modules/Exchange/Controller/ExchangeViewController.swift
+++ CurrencyExchange/CurrencyExchange/Modules/Exchange/Controller/ExchangeViewController.swift
@@ -23,7 +23,7 @@ class ExchangeViewController: UIViewController {
     
     var viewModel: ExchangeViewModel
     
-    private let exchangeView: ExchangeView = {
+    private var exchangeView: ExchangeView = {
         let view = ExchangeView(frame: .zero)
         return view
     }()
@@ -31,6 +31,9 @@ class ExchangeViewController: UIViewController {
     init(viewModel: ExchangeViewModel){
         self.viewModel = viewModel
         super.init(nibName: nil, bundle: nil)
+        
+        self.viewModel.delegate = self
+        
         self.exchangeView.originCurrencyButton.addTarget(self, action: #selector(tappedOnOriginButton), for: .touchUpInside)
         
         self.exchangeView.destinationCurrencyButton.addTarget(self, action: #selector(tappedOnDestinationButton), for: .touchUpInside)
@@ -61,7 +64,7 @@ class ExchangeViewController: UIViewController {
     }
     
     @objc private func tappedOnConverterButton(){
-        self.viewModel.converter()
+        self.viewModel.executeConverterWithExchangeValue(exchangeView.exchangeTextField.text)
     }
     
     // MARK: - Methods
@@ -73,13 +76,40 @@ class ExchangeViewController: UIViewController {
     func setupDestinationButton(){
         self.exchangeView.destinationCurrencyButton.setTitle(viewModel.destinationCurrency?.code, for: .normal)
     }
-    
-    private func converter(){
-        
-    }
+
     
 
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
+    }
+}
+
+extension ExchangeViewController: ExchangeViewModelDelegate {
+    
+    func willBeginCurrencyExchange() {
+        self.exchangeView.showLoadingIndicator(view: exchangeView)
+    }
+    
+    func didFinishCurrencyExchange(message: String?, convertedValue: Double?) {
+        if let message = message {
+            showErrorWithTitle("Erro", withMessage: message)
+        }else if let convertedValue = convertedValue {
+            DispatchQueue.main.async {
+                guard let originCurrency = self.viewModel.destinationCurrency else {
+                    return
+                }
+                let convertedValueString = "\(originCurrency.code): \(String(format: "%.2f", convertedValue))"
+                self.exchangeView.resultValueOfExchangeLabel.text = convertedValueString
+            }
+        }
+        
+        DispatchQueue.main.async { [weak self] in
+            guard let self = self else { return }
+            self.exchangeView.dismissLoadingIndicator()
+        }
+    }
+    
+    func handleConverterError(message: String) {
+        showErrorWithTitle("Erro", withMessage: message)
     }
 }
