bc5f0b4c22a0192606291cd60758ac59f9ead515
Carlos <carloshenriquenlb@gmail.com>
2020-11-01 19:31:53 -0300

    Setup exchange error

---
+++ CurrencyExchange/CurrencyExchange/Modules/Exchange/HandleError/ExchangeError.swift
@@ -0,0 +1,30 @@
+//
+//  ExchangeError.swift
+//  CurrencyExchange
+//
+//  Created by Carlos Fontes on 01/11/20.
+//
+
+import Foundation
+
+enum ExchangeError: Error {
+    case exchangeValueCannotBeUnwrapped
+    case currencyCannotBeUnwrapped
+    case currenciesAreEqual
+}
+
+extension ExchangeError: ErrorDescriptable {
+    var description: String {
+        switch self {
+        
+        case .exchangeValueCannotBeUnwrapped:
+            return "Houve algum erro no valor informado! Por favor tente novamente..."
+        case .currencyCannotBeUnwrapped:
+            return "Erro ao tentar desembrulhar alguma moeda, por favor verifique se as duas moedas foram selecionadas."
+        case .currenciesAreEqual:
+            return "Por favor verifique se você selecionou moedas iguais para origem e destino"
+        }
+    }
+    
+    
+}
