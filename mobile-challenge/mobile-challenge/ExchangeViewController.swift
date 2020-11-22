f61551740926ad17f44f20999380f0cbc6144efe
Bernardo Nunes <bernardonunes97@gmail.com>
2020-11-22 10:04:33 -0300

    create: basic project structure

--- mobile-challenge/mobile-challenge/ExchangeViewController.swift
+++ mobile-challenge/mobile-challenge/ExchangeViewController.swift
@@ -7,10 +7,11 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ExchangeViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        view.backgroundColor = .red
         // Do any additional setup after loading the view.
     }
 
