1e847db2a9a486bcf9b5b26b4d66f2b4d00694dd
Bernardo Nunes <bernardonunes97@gmail.com>
2020-11-22 19:52:43 -0300

    create: CurrencyTableViewDelegate

---
+++ mobile-challenge/mobile-challenge/CurrencyListScreen/Manager/CurrencyTableViewManager+Delegate.swift
@@ -0,0 +1,15 @@
+//
+//  CurrencyTableViewManager+Delegate.swift
+//  mobile-challenge
+//
+//  Created by Bernardo Nunes on 22/11/20.
+//
+
+import Foundation
+import UIKit
+
+extension CurrencyTableViewManager: UITableViewDelegate {
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        
+    }
+}
