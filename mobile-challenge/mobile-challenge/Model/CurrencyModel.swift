1043381b2ef8d195f9013f06899b590336336e78
Bernardo Nunes <bernardonunes97@gmail.com>
2020-11-22 15:08:44 -0300

    create: CurrencyModel

---
+++ mobile-challenge/mobile-challenge/Model/CurrencyModel.swift
@@ -0,0 +1,14 @@
+//
+//  CurrencyModel.swift
+//  mobile-challenge
+//
+//  Created by Bernardo Nunes on 22/11/20.
+//
+
+import Foundation
+
+struct CurrencyModel {
+    let code: String
+    let name: String
+    let value: Double
+}
