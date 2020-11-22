e0a60673a83374714f2d10da5e86fdaa53e4b978
Bernardo Nunes <bernardonunes97@gmail.com>
2020-11-22 10:59:44 -0300

    create: ExchangeView properties

--- mobile-challenge/mobile-challenge/ExchangeScreen/View/ExchangeView.swift
+++ mobile-challenge/mobile-challenge/ExchangeScreen/View/ExchangeView.swift
@@ -10,6 +10,38 @@ import UIKit
 
 class ExchangeView: UIView {
     
+    var fromTextField: UITextField {
+        let textField = UITextField()
+        return textField
+    }
+    
+    var resultLabel: UILabel {
+        let label = UILabel()
+        return label
+    }
+    
+    var fromButton: UIButton {
+        let button = UIButton()
+        return button
+    }
+    
+    var resultButton: UIButton {
+        let button = UIButton()
+        return button
+    }
+    
+    var exchangeStackView: UIStackView {
+        let stackView = UIStackView()
+        return stackView
+    }
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
 }
 
 extension ExchangeView: ViewCodable {
