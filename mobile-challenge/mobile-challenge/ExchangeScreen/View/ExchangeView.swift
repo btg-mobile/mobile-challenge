941aaf2c53809e21501e33923b0ac0e1d8030a9a
Bernardo Nunes <bernardonunes97@gmail.com>
2020-11-22 10:43:17 -0300

    create: ExchangeView

---
+++ mobile-challenge/mobile-challenge/ExchangeScreen/View/ExchangeView.swift
@@ -0,0 +1,23 @@
+//
+//  ExchangeView.swift
+//  mobile-challenge
+//
+//  Created by Bernardo Nunes on 22/11/20.
+//
+
+import Foundation
+import UIKit
+
+class ExchangeView: UIView {
+    
+}
+
+extension ExchangeView: ViewCodable {
+    func setupConstraints() {
+        
+    }
+    
+    func setupViewHierarchy() {
+        
+    }
+}
