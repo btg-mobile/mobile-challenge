e595554971d95dbaf0d8e627fb8ea3f4c74a6660
Caio Azevedo <caioba19@gmail.com>
2020-11-25 20:06:05 -0300

    Create Network Manager and Quotation Model

---
+++ mobile-challenge/mobile-challenge/Quotation/Model/Quotation.swift
@@ -0,0 +1,14 @@
+//
+//  QuotationModel.swift
+//  mobile-challenge
+//
+//  Created by Caio Azevedo on 25/11/20.
+//
+
+import Foundation
+
+struct Quotation: Codable {
+    var success: Bool
+    var timestamp: Int
+    var quotes: [String: Double]
+}
