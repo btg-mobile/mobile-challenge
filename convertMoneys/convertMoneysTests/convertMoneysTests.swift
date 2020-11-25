6d8de65d008d5725f05c6229c3f6e8ea35e6c18e
mateusRodriguesSantos <mateus.rteu@gmail.com>
2020-11-25 10:08:56 -0300

    Xcode Project

---
+++ convertMoneys/convertMoneysTests/convertMoneysTests.swift
@@ -0,0 +1,33 @@
+//
+//  convertMoneysTests.swift
+//  convertMoneysTests
+//
+//  Created by Mateus Rodrigues Santos on 25/11/20.
+//
+
+import XCTest
+@testable import convertMoneys
+
+class convertMoneysTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
