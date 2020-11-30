57155c863fbe0b1efd47f0900c899c37ccdf18e6
Gois <matheusgoislimasilva@gmail.com>
2020-11-29 23:57:54 -0300

    sleep para deixar a screen com um delay

--- app-mobile-challenge/app-mobile-challenge/App/AppDelegate.swift
+++ app-mobile-challenge/app-mobile-challenge/App/AppDelegate.swift
@@ -12,6 +12,7 @@ import CoreData
 class AppDelegate: UIResponder, UIApplicationDelegate {
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        sleep(1)
         return true
     }
 
