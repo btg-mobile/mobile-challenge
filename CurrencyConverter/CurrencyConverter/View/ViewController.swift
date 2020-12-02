748fe6038cb1ab12cb3c4c3c08ecc589aa073674
Isnard Silva <isnardsilva@icloud.com>
2020-12-01 23:39:20 -0300

    Create base project

---
+++ CurrencyConverter/CurrencyConverter/View/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CurrencyConverter
+//
+//  Created by Isnard Silva on 01/12/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
