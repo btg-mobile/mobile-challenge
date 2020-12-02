d97731906a853d7c70e91532ab63be0c0506f4ac
Isnard Silva <isnardsilva@icloud.com>
2020-12-02 15:03:54 -0300

    Setup CurrencyConverterViewController

--- CurrencyConverter/CurrencyConverter/View/CurrencyConverter/CurrencyConverterViewController.swift
+++ CurrencyConverter/CurrencyConverter/View/CurrencyConverter/CurrencyConverterViewController.swift
@@ -25,6 +25,8 @@ class CurrencyConverterViewController: UIViewController {
         super.viewDidLoad()
         
         setupNavigationBar()
+        setupButtons()
+        setupTextFields()
     }
 }
 
@@ -33,5 +35,61 @@ class CurrencyConverterViewController: UIViewController {
 extension CurrencyConverterViewController {
     private func setupNavigationBar() {
         navigationItem.title = "Currency Converter"
+    }
+}
+
+// MARK: - Setup Buttons
+extension CurrencyConverterViewController {
+    private func setupButtons() {
+        baseView.sourceCurrencyTypeButton.addTarget(self, action: #selector(currencyTypeDidTouch(_:)), for: .touchUpInside)
+        baseView.targetCurrencyTypeButton.addTarget(self, action: #selector(currencyTypeDidTouch(_:)), for: .touchUpInside)
+        baseView.conversionButton.addTarget(self, action: #selector(convertCurrenciesButtonDidTouch), for: .touchUpInside)
+    }
+    
+    private func setupTextFields() {
+        baseView.sourceCurrencyValueTextField.addTarget(self, action: #selector(sourceCurrencyTextFieldDidChange(_:)), for: .editingChanged)
+        baseView.targetCurrencyValueTextField.addTarget(self, action: #selector(targetCurrencyTextFieldDidTouch), for: .touchDown)
+    }
+}
+
+
+// MARK: - Actions
+extension CurrencyConverterViewController {
+    @objc private func currencyTypeDidTouch(_ sender: UIButton) {
+        // Identify type button
+        if sender.tag == CurrencyTypeButtonTag.sourceCurrency.rawValue {
+            print("Moeda origem")
+        } else {
+            print("Moeda destino")
+        }
+    }
+    
+    @objc private func sourceCurrencyTextFieldDidChange(_ textField: UITextField) {
+        // Checando se algo foi digitado
+        guard let typedText = textField.text else {
+            print("Digite o valor a ser convertido")
+            return
+        }
+        
+        // Checando se é um número
+        guard let typedValue = Double(typedText) else {
+            print("Digite um valor numérico")
+            return
+        }
+        
+        print("Typed value:", typedValue)
+    }
+    
+    @objc private func targetCurrencyTextFieldDidTouch() {
+        print("targetCurrencyTextFieldDidTouch")
+    }
+    
+    @objc private func convertCurrenciesButtonDidTouch() {
+        do {
+            let resultOfConversion = try viewModel.convertCurrencies()
+            baseView.targetCurrencyValueTextField.text = String(resultOfConversion)
+        } catch {
+            print(error.localizedDescription)
+        }
     }
 }
