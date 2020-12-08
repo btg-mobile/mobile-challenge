f0cc9cbe8d17ee142454c4db78294c16940b7853
leon walras <leongdev@gmail.com>
2020-12-08 10:30:21 -0300

    Adding SwiftLint and Lottie-ios

---
+++ EasyMoneyExchanger/Pods/Target Support Files/Pods-EasyMoneyExchanger-EasyMoneyExchangerUITests/Pods-EasyMoneyExchanger-EasyMoneyExchangerUITests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_EasyMoneyExchanger_EasyMoneyExchangerUITests : NSObject
+@end
+@implementation PodsDummy_Pods_EasyMoneyExchanger_EasyMoneyExchangerUITests
+@end
---
+++ EasyMoneyExchanger/Pods/Target Support Files/Pods-EasyMoneyExchangerTests/Pods-EasyMoneyExchangerTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_EasyMoneyExchangerTests : NSObject
+@end
+@implementation PodsDummy_Pods_EasyMoneyExchangerTests
+@end
