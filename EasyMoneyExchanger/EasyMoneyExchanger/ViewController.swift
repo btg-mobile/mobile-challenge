53ba9dd93cee596a89a3329c7f09e1dbceb4b0b4
leon walras <leongdev@gmail.com>
2020-12-07 21:52:25 -0300

    Setup project

---
+++ EasyMoneyExchanger/EasyMoneyExchanger/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  EasyMoneyExchanger
+//
+//  Created by Leon on 07/12/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
