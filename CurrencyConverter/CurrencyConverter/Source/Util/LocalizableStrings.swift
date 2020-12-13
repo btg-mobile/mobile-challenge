258dd8110e1f3caa62e24ebd7cc01b7807158f30
Italo Boss <italoboss@gmail.com>
2020-12-13 10:09:33 -0300

    Add LocalizableStrings enum and values

---
+++ CurrencyConverter/CurrencyConverter/Source/Util/LocalizableStrings.swift
@@ -0,0 +1,32 @@
+//
+//  LocalizableStrings.swift
+//  CurrencyConverter
+//
+//  Created by Italo Boss on 13/12/20.
+//
+
+import Foundation
+
+enum LocalizableStrings: String {
+    
+    case generalLoadingText
+    case generalCancelText
+    case generalEqualSign
+    
+    case currencyConverterViewTitle
+    case currencyConverterViewValuePlaceholder
+    case currencyConverterViewLastUpdatedTaxes
+    
+}
+
+extension LocalizableStrings {
+    
+    var localized: String {
+        return NSLocalizedString(self.rawValue, comment: "")
+    }
+    
+    func localizedWith(_ values: CVarArg...) -> String {
+        return String(format: self.localized, arguments: values)
+    }
+
+}
