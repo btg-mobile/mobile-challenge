56ec2327fc007228fffa2438f18dd28f620fa986
Italo Boss <italoboss@gmail.com>
2020-12-13 14:10:58 -0300

    Add CurrencyLayerService implementation

---
+++ CurrencyConverter/CurrencyConverter/Source/Service/CurrencyLayerService.swift
@@ -0,0 +1,69 @@
+//
+//  CurrencyLayerService.swift
+//  CurrencyConverter
+//
+//  Created by Italo Boss on 13/12/20.
+//
+
+import Foundation
+
+enum CurrencyLayerService: Service {
+    
+    case list
+    case live(from: String, to: String)
+    
+    var baseURL: URL {
+        guard let url = URL(string: "http://api.currencylayer.com")
+            else {
+                return URL(fileURLWithPath: "")
+        }
+        return url
+    }
+    
+    var path: String {
+        switch self {
+        case .list:
+            return "/list"
+        case .live:
+            return "/live"
+        }
+    }
+    
+    var method: HTTPMethod {
+        switch self {
+        case .list, .live:
+            return .get
+        }
+    }
+    
+    var task: HTTPTask {
+        let accessKey = ""
+        
+        switch self {
+        case .list:
+            return .requestParameters(["access_key": accessKey])
+        case .live(let fromCurrency, let toCurrency):
+            return .requestParameters(["access_key": accessKey, "source": fromCurrency, "currencies": toCurrency])
+        }
+    }
+    
+    var headers: Headers? {
+        switch self {
+        case .list, .live:
+            return ["Accept": "application/json"]
+        }
+    }
+    
+    var parametersEncoding: ParametersEncoding {
+        switch self {
+        case .list, .live:
+            return .url
+        }
+    }
+    
+    // MARK: - API Access Key
+    var accessKey: String {
+        return ""
+    }
+    
+}
