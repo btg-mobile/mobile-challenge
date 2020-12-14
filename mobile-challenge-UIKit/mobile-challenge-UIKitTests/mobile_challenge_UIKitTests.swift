46b5c4c63a141aed7f74cb38aab08fc168e28b2d
Lucas Fernandez Nicolau <dev.lucasfn@gmail.com>
2020-12-14 19:13:22 -0300

    First commit and .gitignore

---
+++ mobile-challenge-UIKit/mobile-challenge-UIKitTests/mobile_challenge_UIKitTests.swift
@@ -0,0 +1,33 @@
+//
+//  mobile_challenge_UIKitTests.swift
+//  mobile-challenge-UIKitTests
+//
+//  Created by Lucas Fernandez Nicolau on 14/12/20.
+//
+
+import XCTest
+@testable import mobile_challenge_UIKit
+
+class mobile_challenge_UIKitTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
