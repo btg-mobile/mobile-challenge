825c6a1778e95cadd5db66e10aa1610cf9ac6f5e
Lucas Fernandez Nicolau <dev.lucasfn@gmail.com>
2020-12-14 19:28:45 -0300

    Remove Main.storyboard; organize files into folders; setup the project to use viewcode

--- mobile-challenge-UIKit/mobile-challenge-UIKit/Controllers/CurrencyConverterViewController.swift
+++ mobile-challenge-UIKit/mobile-challenge-UIKit/Controllers/CurrencyConverterViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  CurrencyConverterViewController.swift
 //  mobile-challenge-UIKit
 //
 //  Created by Lucas Fernandez Nicolau on 14/12/20.
@@ -7,13 +7,11 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class CurrencyConverterViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
-
 
 }
 
---
+++ mobile-challenge-UIKit/mobile-challenge-UIKit/Supporting Files/SceneDelegate.swift
@@ -0,0 +1,32 @@
+//
+//  SceneDelegate.swift
+//  mobile-challenge-UIKit
+//
+//  Created by Lucas Fernandez Nicolau on 14/12/20.
+//
+
+import UIKit
+
+class SceneDelegate: UIResponder, UIWindowSceneDelegate {
+
+    var window: UIWindow?
+
+    func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
+
+        guard let scene = (scene as? UIWindowScene) else { return }
+        window = UIWindow(windowScene: scene)
+        window?.rootViewController = CurrencyConverterViewController()
+        window?.makeKeyAndVisible()
+    }
+
+    func sceneDidDisconnect(_ scene: UIScene) {}
+
+    func sceneDidBecomeActive(_ scene: UIScene) {}
+
+    func sceneWillResignActive(_ scene: UIScene) {}
+
+    func sceneWillEnterForeground(_ scene: UIScene) {}
+
+    func sceneDidEnterBackground(_ scene: UIScene) {}
+}
+
