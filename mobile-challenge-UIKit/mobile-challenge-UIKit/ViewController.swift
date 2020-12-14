e659d1ad6fd6a365989e09571895d12904a2354a
Lucas Fernandez Nicolau <dev.lucasfn@gmail.com>
2020-12-14 19:32:32 -0300

    First commit and .gitignore

---
+++ mobile-challenge-UIKit/mobile-challenge-UIKit/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  mobile-challenge-UIKit
+//
+//  Created by Lucas Fernandez Nicolau on 14/12/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
