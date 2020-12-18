9a709f6b607ea872ab0b014e71ae3ea74983fb21
pedrohgsilveira <pedrohgsilveira@gmail.com>
2020-12-18 19:15:16 -0300

    Adds CurrencyAPI Struct

---
+++ BTG_Mobile_Challenge/BTG_Mobile_Challenge/Source/Network/CurrencyAPI.swift
@@ -0,0 +1,13 @@
+//
+//  CurrencyAPI.swift
+//  BTG_Mobile_Challenge
+//
+//  Created by Pedro Henrique Guedes Silveira on 18/12/20.
+//
+
+import Foundation
+
+struct CurrencyAPI {
+    static let url = "http://api.currencylayer.com"
+    static let accessKey = "0bda9a8ceb08a7ea5ad4217c645ee12a"
+}
