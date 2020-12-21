ddc7191ea5f1a9f44cb43cce40e79df96807cbe2
pedrohgsilveira <pedrohgsilveira@gmail.com>
2020-12-21 09:15:57 -0300

    Final fixes

--- BTG_Mobile_Challenge/BTG_Mobile_Challenge/Helpers/Extensions/Array+Extension.swift
+++ BTG_Mobile_Challenge/BTG_Mobile_Challenge/Helpers/Extensions/Array+Extension.swift
@@ -8,15 +8,12 @@
 import Foundation
 
 extension Array where Element: Hashable {
+    
     func removingDuplicates() -> [Element] {
         var addedDict = [Element: Bool]()
 
         return filter {
             addedDict.updateValue(true, forKey: $0) == nil
         }
-    }
-
-    mutating func removeDuplicates() {
-        self = self.removingDuplicates()
     }
 }
--- BTG_Mobile_Challenge/BTG_Mobile_Challenge/Helpers/Extensions/UIAlert+Extension.swift
+++ BTG_Mobile_Challenge/BTG_Mobile_Challenge/Helpers/Extensions/UIAlert+Extension.swift
@@ -9,11 +9,6 @@ import UIKit
 
 extension UIAlertController {
 
-    /**
-     This method adds a single action to a UIAlertController that shows a log message when clicked, also setting the view's tint color to be the app's main color.
-     - Parameter logMessage: A string containing the message to be displayed in the log.
-     - Returns: The customised UIAlertController.
-     */
     internal func makeErrorMessage(with logMessage: String) -> UIAlertController {
         let action = UIAlertAction(title: "Done", style: .default) { _ in
             NSLog(logMessage)
