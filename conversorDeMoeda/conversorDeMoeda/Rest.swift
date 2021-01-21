018c2bc894cf78f81b0d8aaffaea58e8add52ec2
Diogenes <diogenesdesouza@yahoo.com.br>
2021-01-20 21:23:15 -0300

    Correção da interface para iPhone SE e Iphone 7

--- conversorDeMoeda/conversorDeMoeda/Rest.swift
+++ conversorDeMoeda/conversorDeMoeda/Rest.swift
@@ -5,8 +5,6 @@
 //  Created by Diogenes de Souza on 08/01/21.
 //
 
-//classe responsável por fazer a requisição ao servidor
-
 import Foundation
 
 
@@ -15,13 +13,12 @@ enum cambioError {
     case takError(error: Error)
     case noResponse
 }
-//classe responsável por fazer a requisição no servidor
+// MARK: classe responsável por fazer a requisição no servidor
 class Rest {
     private static let basePhath = "http://api.currencylayer.com/"
-    private static let acesskey = "?access_key=62ac3fd83ede8cb37b1591c7d1635ae4" //chave da api
+    private static let acesskey = "?access_key=62ac3fd83ede8cb37b1591c7d1635ae4" // MARK: chave da api
     private static var fullUrl: URL?
     
-    //configurar sessao
     private static let configuration : URLSessionConfiguration = {
         
         let config = URLSessionConfiguration.default
@@ -32,17 +29,15 @@ class Rest {
         return config
     }()
     
-    //Carrega a lista de moedas com nome e siglas correspondentes (String:String)
+    // MARK: Carrega a lista de moedas com nome e siglas correspondentes (String:String)
     class func loadCurrencys(endPoint: String, onClomplete: @escaping(Dictionary<String,String>) -> Void, onError: @escaping(cambioError) -> Void){
-        
-        //define o endPoint da url
+    
         fullUrl = URL(string: basePhath + endPoint + acesskey)
         guard let url = fullUrl else {
             onError(.url)
             return
-            
         }
-        //tarefa e resposta com URLResponse, variavel data
+       
         let dataTask = URLSession.shared.dataTask(with: url) { (data:Data?, response:URLResponse?, error:Error?) in
             if error == nil{
                 guard let response = response as? HTTPURLResponse else{
@@ -50,16 +45,15 @@ class Rest {
                     return
                 }
                 
-                //se houve sucesso na resposta código 200
+                // MARK: se houve sucesso na resposta código 200
                 if response.statusCode == 200 {
                     guard let data = data else{return}
                     
                     do{
                         
-                        //decodificar JSON
+                        // MARK: decodificar JSON
                         let cambioList: Cambio = try JSONDecoder().decode(Cambio.self, from: data)
                         
-                        //Dicionário de moedas para consulta( chave:valor)
                         let arrayDeMoedas: Dictionary = cambioList.currencies
                         print("\narray de moedas list: \(arrayDeMoedas)")
                         
@@ -81,46 +75,33 @@ class Rest {
         
     }
     
-    //Carrega a lista de moedas com siglas e cotações correspondentes (String:Double)
+    // MARK: Carrega a lista de moedas com siglas e cotações correspondentes (String:Double)
     class func loadCurrencysValues(endPoint: String, onClomplete: @escaping(Dictionary<String,Double>) -> Void, onError: @escaping(cambioError) -> Void){
         
-        //define o endPoint da url
         fullUrl = URL(string: basePhath + endPoint + acesskey)
         
         guard let url = fullUrl else {
             onError(.url)
             return
-            
         }
-        //tarefa e resposta com URLResponse, variavel data
         let dataTask = URLSession.shared.dataTask(with: url) { (data:Data?, response:URLResponse?, error:Error?) in
             if error == nil{
                 guard let response = response as? HTTPURLResponse else{
                     onError(.noResponse)
                     return
-                    
                 }
                 if response.statusCode == 200 {
                     guard let data = data else{return}
                     
                     do{
-                        
                         let cambioList: Cotacao = try JSONDecoder().decode(Cotacao.self, from: data)
-                        
                         let arrayDeMoedas = cambioList.quotes
                         print("\narray de moedas list: \(arrayDeMoedas)")
-                        
-            
-                        
                         onClomplete(arrayDeMoedas)
-                        
-                        
-                    }catch{
+                        }catch{
                         print(error.localizedDescription)
-                        
-                    }
-                    
-                }else{
+                        }
+                    }else{
                     print("Algum status inválido no servidor")
                 }
             }else{
@@ -135,17 +116,16 @@ class Rest {
     
     
     
-    //Carrega a lista de moedas com nome e siglas correspondentes (String:String)
+    // MARK: Carrega a lista de moedas com nome e siglas correspondentes (String:String)
     class func carregarLista(endPoint: String, onClomplete: @escaping(Dictionary<String,String>) -> Void, onError: @escaping(cambioError) -> Void){
         
-        //define o endPoint da url
         fullUrl = URL(string: basePhath + endPoint + acesskey)
         guard let url = fullUrl else {
             onError(.url)
             return
             
         }
-        //tarefa e resposta com URLResponse, variavel data
+      
         let dataTask = URLSession.shared.dataTask(with: url) { (data:Data?, response:URLResponse?, error:Error?) in
             if error == nil{
                 guard let response = response as? HTTPURLResponse else{
