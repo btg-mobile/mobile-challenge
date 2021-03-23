9493d08a15e281842968476e8543d6fcfa909c73
anascim <alexjnascim@gmail.com>
2021-03-22 21:42:52 -0300

    remove Storyboard

--- CurrencyConverterBTG/CurrencyConverterBTG/AppDelegate.swift
+++ CurrencyConverterBTG/CurrencyConverterBTG/AppDelegate.swift
@@ -10,10 +10,14 @@ import UIKit
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
-
+    var window: UIWindow?
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        // Support for iOS 12 and below
+        window = UIWindow()
+        window?.rootViewController = ViewController()
+        window?.makeKeyAndVisible()
         return true
     }
 
--- CurrencyConverterBTG/CurrencyConverterBTG/ViewController.swift
+++ CurrencyConverterBTG/CurrencyConverterBTG/ViewController.swift
@@ -11,7 +11,15 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
         // Do any additional setup after loading the view.
+        self.view.backgroundColor = .red
+        self.view.tintColor = .green
+        
+        let button = UIButton(frame: CGRect(x: 30, y: 30, width: 30, height: 30))
+        button.tintColor = .gray
+        button.backgroundColor = .brown
+        self.view.addSubview(button)
     }
 
 
