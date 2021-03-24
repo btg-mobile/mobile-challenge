c6ab987725b27068a2915d66538a4d4c9bf930d0
anascim <alexjnascim@gmail.com>
2021-03-23 22:41:18 -0300

    color pallete and visual adjustments

--- CurrencyConverterBTG/CurrencyConverterBTG/Utils/DesignSystem.swift
+++ CurrencyConverterBTG/CurrencyConverterBTG/Utils/DesignSystem.swift
@@ -7,12 +7,35 @@
 
 import UIKit
 
+/**
+ Class for centralizing the design patterns in one place.
+ All it's properties should be static.
+ */
 class DesignSystem {
     
     /// Padding from the LayoutMarguinsGuide to the UI elements
-    static let marginsPadding: CGFloat = 40
+    static let marginsPadding: CGFloat = 10
     
-    static func getButtonWidth(view: UIView) -> CGFloat {
-        return view.frame.width/5
+    /// Color palett base on: https://colorhunt.co/palette/264850
+    struct Color {
+        static var primary: UIColor = #colorLiteral(red: 0.168627451, green: 0.1803921569, blue: 0.2901960784, alpha: 1)
+        static var secondary: UIColor = #colorLiteral(red: 0.9098039216, green: 0.2705882353, blue: 0.2705882353, alpha: 1)
+        static var tertiary: UIColor = #colorLiteral(red: 0.5647058824, green: 0.2156862745, blue: 0.2862745098, alpha: 1)
+        static var quaternary: UIColor = #colorLiteral(red: 0.3254901961, green: 0.2078431373, blue: 0.2901960784, alpha: 1)
+        static var white: UIColor = #colorLiteral(red: 1.0, green: 1.0, blue: 1.0, alpha: 1.0)
+    }
+    
+    struct Button {
+        static func getWidth(view: UIView) -> CGFloat {
+            return view.frame.width/3
+        }
+        
+        static func getHeight(view: UIView) -> CGFloat {
+            return view.frame.width/4
+        }
+        
+        static func getCornerRadius(view: UIView) -> CGFloat {
+            return getHeight(view: view)/6
+        }
     }
 }
--- CurrencyConverterBTG/CurrencyConverterBTG/View/ConversionsViewController.swift
+++ CurrencyConverterBTG/CurrencyConverterBTG/View/ConversionsViewController.swift
@@ -26,12 +26,26 @@ final class ConversionsViewController: UIViewController {
     }
     
     override func viewDidLoad() {
+        view.backgroundColor = DesignSystem.Color.primary
+        
         originBtn.setTitle("Origin", for: .normal)
+        originBtn.tintColor = DesignSystem.Color.white
+        originBtn.titleLabel?.numberOfLines = 0
+        originBtn.titleLabel?.textAlignment = .center
         originBtn.addTarget(self, action: #selector(didSelectOrigin), for: .touchUpInside)
+        originBtn.backgroundColor = DesignSystem.Color.secondary
+        originBtn.layer.cornerRadius = DesignSystem.Button.getCornerRadius(view: view)
         
         destinyBtn.setTitle("Destiny", for: .normal)
+        destinyBtn.tintColor = DesignSystem.Color.white
+        destinyBtn.titleLabel?.numberOfLines = 0
+        destinyBtn.titleLabel?.textAlignment = .center
         destinyBtn.addTarget(self, action: #selector(didSelectDestiny), for: .touchUpInside)
+        destinyBtn.backgroundColor = DesignSystem.Color.secondary
+        destinyBtn.layer.cornerRadius = DesignSystem.Button.getCornerRadius(view: view)
+        
         conversionArrow.image = UIImage(systemName: "arrow.right")
+        conversionArrow.tintColor = DesignSystem.Color.tertiary
         
         view.addSubview(originBtn)
         view.addSubview(destinyBtn)
@@ -53,10 +67,14 @@ final class ConversionsViewController: UIViewController {
             originBtn.leftAnchor.constraint(equalTo: view.layoutMarginsGuide.leftAnchor, constant: DesignSystem.marginsPadding),
             destinyBtn.rightAnchor.constraint(equalTo: view.layoutMarginsGuide.rightAnchor, constant: -DesignSystem.marginsPadding),
             originBtn.bottomAnchor.constraint(equalTo: view.layoutMarginsGuide.bottomAnchor),
-            originBtn.widthAnchor.constraint(equalToConstant: DesignSystem.getButtonWidth(view: view)),
-            destinyBtn.widthAnchor.constraint(equalToConstant: DesignSystem.getButtonWidth(view: view)),
+            originBtn.widthAnchor.constraint(equalToConstant: DesignSystem.Button.getWidth(view: view)),
+            originBtn.heightAnchor.constraint(equalToConstant: DesignSystem.Button.getHeight(view: view)),
+            destinyBtn.widthAnchor.constraint(equalToConstant: DesignSystem.Button.getWidth(view: view)),
+            destinyBtn.heightAnchor.constraint(equalToConstant: DesignSystem.Button.getHeight(view: view)),
             conversionArrow.centerYAnchor.constraint(equalTo: originBtn.centerYAnchor),
-            conversionArrow.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            conversionArrow.leftAnchor.constraint(equalTo: originBtn.rightAnchor, constant: DesignSystem.marginsPadding),
+            conversionArrow.rightAnchor.constraint(equalTo: destinyBtn.leftAnchor, constant: -DesignSystem.marginsPadding),
+            conversionArrow.widthAnchor.constraint(equalTo: conversionArrow.heightAnchor),
             destinyBtn.centerYAnchor.constraint(equalTo: originBtn.centerYAnchor)
         ])
     }
