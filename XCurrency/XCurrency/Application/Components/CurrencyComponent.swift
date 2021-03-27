65c541067eed00dea15561a328b5e6a22ca57879
Vinicius Nadin <vininadin@gmail.com>
2021-03-27 16:21:40 -0300

    Currency Component Implementation

---
+++ XCurrency/XCurrency/Application/Components/CurrencyComponent.swift
@@ -0,0 +1,39 @@
+//
+//  CurrencyComponent.swift
+//  XCurrency
+//
+//  Created by Vinicius Nadin on 27/03/21.
+//
+
+import UIKit
+
+class CurrencyComponent: UIView {
+    override func awakeAfter(using coder: NSCoder) -> Any? {
+        return self.loadFromNibIfEmbeddedInDifferentNib()
+    }
+}
+
+extension UIView {
+  func loadFromNibIfEmbeddedInDifferentNib() -> Self {
+    let isJustAPlaceholder = subviews.count == 0
+    if isJustAPlaceholder {
+      let theRealThing = type(of: self).viewFromNib()
+      theRealThing.frame = frame
+      translatesAutoresizingMaskIntoConstraints = false
+      theRealThing.translatesAutoresizingMaskIntoConstraints = false
+      return theRealThing
+    }
+    return self
+  }
+}
+
+extension UIView {
+  class func viewFromNib(withOwner owner: Any? = nil) -> Self {
+    let name = String(describing: type(of:      self)).components(separatedBy: ".")[0]
+    let view = UINib(nibName: name, bundle: nil).instantiate(withOwner: owner, options: nil)[0]
+    return cast(view)!
+  }
+}
+private func cast<T, U>(_ value: T) -> U? {
+  return value as? U
+}
