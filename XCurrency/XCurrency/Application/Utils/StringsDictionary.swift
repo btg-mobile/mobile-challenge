847b1bfbf8226e6a182cebe54f1a4abeefb73083
Vinicius Nadin <vininadin@gmail.com>
2021-03-28 18:21:33 -0300

    StringsDictionary Implementation

---
+++ XCurrency/XCurrency/Application/Utils/StringsDictionary.swift
@@ -0,0 +1,13 @@
+//
+//  StringsDictionary.swift
+//  XCurrency
+//
+//  Created by Vinicius Nadin on 28/03/21.
+//
+
+struct StringsDictionary {
+    static let back = "Back"
+    static let firstCurrency = "First Currency"
+    static let secondCurrency = "Second Currency"
+    static let currencyConverter = "Currency converter"
+}
