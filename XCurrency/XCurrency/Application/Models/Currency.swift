68f13139b7b0f974ffe69de22603e0324e96dab0
Vinicius Nadin <vininadin@gmail.com>
2021-03-28 17:32:10 -0300

    Created currency order file

---
+++ XCurrency/XCurrency/Application/Models/Currency.swift
@@ -0,0 +1,11 @@
+//
+//  Currency.swift
+//  XCurrency
+//
+//  Created by Vinicius Nadin on 28/03/21.
+//
+
+struct Currency: Codable {
+    let name: String
+    let code: String
+}
---
+++ XCurrency/XCurrency/Application/Models/CurrencyOrder.swift
@@ -0,0 +1,11 @@
+//
+//  CurrencyOrder.swift
+//  XCurrency
+//
+//  Created by Vinicius Nadin on 28/03/21.
+//
+
+enum CurrencyOrder {
+    case first
+    case second
+}
