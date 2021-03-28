c30764090076291a717dadd6a0091adb90a5ce02
Vinicius Nadin <vininadin@gmail.com>
2021-03-28 17:32:59 -0300

    Improoved currency component

--- XCurrency/XCurrency/Application/Components/CurrencysButton/CurrencyComponent.swift
+++ XCurrency/XCurrency/Application/Components/CurrencysButton/CurrencyComponent.swift
@@ -8,32 +8,23 @@
 import UIKit
 
 class CurrencyComponent: UIView {
-    override func awakeAfter(using coder: NSCoder) -> Any? {
-        return self.loadFromNibIfEmbeddedInDifferentNib()
-    }
-}
 
-extension UIView {
-    func loadFromNibIfEmbeddedInDifferentNib() -> Self {
-        let isJustAPlaceholder = subviews.count == 0
-        if isJustAPlaceholder {
-            let theRealThing = type(of: self).viewFromNib()
-            theRealThing.frame = frame
-            translatesAutoresizingMaskIntoConstraints = false
-            theRealThing.translatesAutoresizingMaskIntoConstraints = false
-            return theRealThing
-        }
-        return self
+    // MARK: - Initialziers
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        self.setup()
     }
-}
 
-extension UIView {
-    class func viewFromNib(withOwner owner: Any? = nil) -> Self {
-        let name = String(describing: type(of:      self)).components(separatedBy: ".")[0]
-        let view = UINib(nibName: name, bundle: nil).instantiate(withOwner: owner, options: nil)[0]
-        return cast(view)!
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+        self.setup()
     }
-}
-private func cast<T, U>(_ value: T) -> U? {
-    return value as? U
+
+    // MARK: - Private Methods
+    private func setup() {
+        let nib = UINib(nibName: "CurrencyComponent", bundle: nil)
+        let view: UIView = nib.instantiate(withOwner: self, options: nil)[0] as! UIView
+        view.frame = self.bounds
+        self.addSubview(view)
+    }
 }
