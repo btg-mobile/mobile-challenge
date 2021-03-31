de81891b264eafeb0f890483868760328d3d5c35
Euclides Medeiros <euclidesmedeiros@MacBook-Pro-de-Euclides.local>
2021-03-31 11:05:31 -0300

    Criando projeto em viewCode

---
+++ Desafio-BTG/Desafio-BTG/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Desafio-BTG
+//
+//  Created by Euclides Medeiros on 31/03/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .blue
+    }
+
+
+}
+
