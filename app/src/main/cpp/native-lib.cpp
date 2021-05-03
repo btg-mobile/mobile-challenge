92a8223d1a444bd602c269726a085edc06a4f9b0
leonardo <leonardo@lanytecnologia.com.br>
2021-05-03 02:00:34 -0300

    add instruction inside native-lib.cpp

--- app/src/main/cpp/native-lib.cpp
+++ app/src/main/cpp/native-lib.cpp
@@ -1,7 +1,10 @@
 /**
  * This is a cpp code to protect sensitive data in a lower level language.
  * Before executing the app, you should update the value of currency_layer_api_key
- * with your own value generated on https://currencylayer.com/
+ * with your own value generated on https://currencylayer.com/.
+ *
+ * after updates use:
+ * git update-index --assume-unchanged app/src/main/cpp/native-lib.cpp
  */
 #include <jni.h>
 #include <string>
