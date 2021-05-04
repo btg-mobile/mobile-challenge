fdd590d43def260d199f3a25e0d40568ccfbe11a
george <geocdias@gmail.com>
2021-05-03 22:45:02 -0300

    Configuração de estrutura de pastas e inclusao de bibliotecas

---
+++ app/src/main/java/com/geocdias/convecurrency/ui/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.geocdias.convecurrency.ui
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.geocdias.convecurrency.R
+
+class MainActivity : AppCompatActivity() {
+  override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(savedInstanceState)
+    setContentView(R.layout.activity_main)
+  }
+}
