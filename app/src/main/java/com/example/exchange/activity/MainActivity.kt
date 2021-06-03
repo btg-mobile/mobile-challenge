8ec4fb01264a7224c85c04ec3a6d425a4853bd5b
Marquinhos <Marquinhos@MacBook-Pro.local>
2021-06-03 00:53:06 -0300

    add layouts

--- app/src/main/java/com/example/exchange/activity/MainActivity.kt
+++ app/src/main/java/com/example/exchange/activity/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.example.exchange
+package com.example.exchange.activity
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import com.example.exchange.R
 
 class MainActivity : AppCompatActivity() {
 
