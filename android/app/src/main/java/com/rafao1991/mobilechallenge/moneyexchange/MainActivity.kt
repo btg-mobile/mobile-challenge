c575fed833af7683a4d9e9f0bf79871a268ddd4b
Rafael Sousa (Rafao) <1c302489@gmail.com>
2021-08-25 20:23:26 -0300

    Retrofit API calls working

--- android/app/src/main/java/com/rafao1991/mobilechallenge/moneyexchange/MainActivity.kt
+++ android/app/src/main/java/com/rafao1991/mobilechallenge/moneyexchange/MainActivity.kt
@@ -2,10 +2,17 @@ package com.rafao1991.mobilechallenge.moneyexchange
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.rafao1991.mobilechallenge.moneyexchange.ui.main.MainFragment
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        if (savedInstanceState == null) {
+            supportFragmentManager.beginTransaction()
+                .replace(R.id.container, MainFragment.newInstance())
+                .commitNow()
+        }
     }
 }
