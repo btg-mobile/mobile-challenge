49f129ba6c725ace405481e14bc8c3bdbe9d5b4e
Júnior Rottava <juniorrottava@hotmail.com>
2021-09-02 16:39:45 -0300

    Project Start

---
+++ coinexchanger/AppDelegate.swift
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.swift
+//  coinexchanger
+//
+//  Created by Junior on 02/09/21.
+//
+
+import UIKit
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        // Override point for customization after application launch.
+        return true
+    }
+}
+
---
+++ coinexchanger/ViewController.swift
@@ -0,0 +1,16 @@
+//
+//  ViewController.swift
+//  coinexchanger
+//
+//  Created by Junior on 02/09/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+}
+
