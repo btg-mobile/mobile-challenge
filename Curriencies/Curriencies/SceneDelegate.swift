440a418f1e3db50339a27cc2d50baf06f8e4d3eb
Gabriel Ferraz <brielff@hotmail.com>
2021-09-03 16:42:20 -0300

    Add factory to sceneDelegate

--- Curriencies/Curriencies/SceneDelegate.swift
+++ Curriencies/Curriencies/SceneDelegate.swift
@@ -15,7 +15,7 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
         guard let windowScene = scene as? UIWindowScene else { return }
         let window = UIWindow(windowScene: windowScene)
-        window.rootViewController = CurrencyHomeViewController()
+        window.rootViewController =  CurrencyHomeFactory.make()
         self.window = window
         window.makeKeyAndVisible()
     }
---
+++ Curriencies/Curriencies/Scenes/CurrencyHome/CurrencyHomeFactory.swift
@@ -0,0 +1,23 @@
+//
+//  CurrencyHomeFactory.swift
+//  Curriencies
+//
+//  Created by Ferraz on 03/09/21.
+//
+
+import UIKit
+
+enum CurrencyHomeFactory {
+    static func make() -> UIViewController {
+        let localData: LocalCurrencyProtocol = LocalCurrency()
+        let apiData: APICurrencyProtocol = APICurrency()
+        let repository: CurrencyRepositoryProtocol = CurrencyRepository(apiCurrency: apiData,
+                                                                        localCurrency: localData)
+        
+        let useCase: GetCurrencyUseCaseProtocol = GetCurrencyUseCase(repository: repository)
+        let viewModel: CurrencyViewModeling = CurrencyViewModel(getCurrenciesUseCase: useCase)
+        let viewController = CurrencyHomeViewController(viewModel: viewModel)
+        
+        return viewController
+    }
+}
