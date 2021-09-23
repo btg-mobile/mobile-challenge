70e6bb258fb0c0d6170007d47c14c313e4d64568
Daive Simões <daives@ciandt.com>
2021-09-23 11:07:04 -0300

    Criação do projeto

---
+++ Mobile Challenge/Mobile Challenge/AppDelegate.swift
@@ -0,0 +1,21 @@
+//
+//  AppDelegate.swift
+//  Mobile Challenge
+//
+//  Created by Daive Costa Nardi Simões on 23/09/21.
+//
+
+import UIKit
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        // Override point for customization after application launch.
+        return true
+    }
+
+}
+
---
+++ Mobile Challenge/Mobile Challenge/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Mobile Challenge
+//
+//  Created by Daive Costa Nardi Simões on 23/09/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
