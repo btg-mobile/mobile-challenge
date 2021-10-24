4ba2275504f715f671659541e8bc53156041d66d
ADRIANO.MAZUCATO <adriano.mazucato@MacBook-Air-de-WAPPA611.local>
2021-10-24 09:36:45 -0300

    Network Layer

--- ChallengerConverter/ChallengerConverter/Network/BTGCurrencyProvider.swift
+++ ChallengerConverter/ChallengerConverter/Network/BTGCurrencyProvider.swift
@@ -6,3 +6,39 @@
 //
 
 import Foundation
+
+
+enum HTTPMethod: String {
+    case GET
+    case POST
+}
+
+
+protocol Provider {
+    var endpoint: String { get }
+    var method: HTTPMethod { get }
+}
+
+fileprivate class Constants {
+    static let baseURL = "http://api.currencylayer.com/"
+    static let accessKey: String = "?access_key=6e0f2a31e5403643184897362ce49c94"
+}
+
+enum ApiProvider: Provider {
+    
+    case live
+    case list
+    
+    var endpoint: String {
+        switch(self) {
+        case .list:
+            return "\(Constants.baseURL)list\(Constants.accessKey)"
+        case .live:
+            return "\(Constants.baseURL)live\(Constants.accessKey)"
+        }
+    }
+    
+    var method: HTTPMethod {
+        return .GET
+    }
+}
--- ChallengerConverter/ChallengerConverter/Network/BTGCurrencyService.swift
+++ ChallengerConverter/ChallengerConverter/Network/BTGCurrencyService.swift
@@ -6,3 +6,42 @@
 //
 
 import Foundation
+
+enum APIError: Error{
+    case error(String)
+}
+
+
+class BTGCurrencyService {
+    
+    static func request<T: Codable>(provider: Provider, completion: @escaping (Result<T, Error>)-> Void) {
+        
+        let task = URLSession.shared.dataTask(with: request(provider: provider)){
+            data, _, error in
+            DispatchQueue.main.async {
+                
+                guard let data = data, error == nil else {
+                    completion(.failure(APIError.error(error!.localizedDescription)))
+                    return
+                }
+                do{
+                    let result = try JSONDecoder().decode(T.self, from: data)
+                    completion(.success(result))
+                } catch(let error) {
+                    completion(.failure(APIError.error(error.localizedDescription)))
+                }
+            }
+        }
+        task.resume()
+    }
+    
+    static private func request(provider: Provider)-> URLRequest {
+        
+        let url = URL(string: provider.endpoint)
+        var request = URLRequest(url: url!)
+        request.httpMethod = provider.method.rawValue
+        request.allHTTPHeaderFields = ["Content-Type": "application/json"]
+
+        return request
+    }
+}
