331fbdc2dfa590be2ec46f00b272f36675e3d152
ADRIANO.MAZUCATO <adriano.mazucato@MacBook-Air-de-WAPPA611.local>
2021-10-24 09:38:37 -0300

    Root coordinator

--- ChallengerConverter/ChallengerConverter/Coordinator/BTGAppCoordinator.swift
+++ ChallengerConverter/ChallengerConverter/Coordinator/BTGAppCoordinator.swift
@@ -6,10 +6,47 @@
 //
 
 import Foundation
+import UIKit
 
+protocol BTGAppCoordinatorDelegate: class {
+    func showPickerCurrencies()
+}
 
 class BTGAppCoordinator: Coordinator {
     
     
+    private lazy var navController: UINavigationController = {
+        let navController = UINavigationController()
+        //navController.setNavigationBarHidden(true, animated: false)
+        return navController
+    }()
     
+    init(window: UIWindow?) {
+        window?.rootViewController = navController
+        window?.makeKeyAndVisible()
+    }
+    
+    
+    func start() {
+        let viewModel = BTGCurrencyConverterViewModel(repository: MockCurrencyRepository())
+        viewModel.coordinatorDelegate = self
+        let viewController = BTGCurrencyConverterViewController(viewModel: viewModel)
+        navController.pushViewController(viewController, animated: false)
+    }
 }
+
+extension BTGAppCoordinator : BTGAppCoordinatorDelegate {
+    func showPickerCurrencies() {
+        let viewModel = BTGCurrenciesAvaliableViewModel(repository: NetworkCurrencyRepository(), delegate: self)
+        let viewController = BTGCurrenciesAvaliableViewController(viewModel: viewModel)
+        navController.pushViewController(viewController, animated: true)
+    }
+}
+
+extension BTGAppCoordinator : BTGCurrenciesAvaliableViewModelDelegate {
+    func didChoiseCurrency(currency: Currency) {
+        navController.popViewController(animated: true)
+    }
+}
+
+
