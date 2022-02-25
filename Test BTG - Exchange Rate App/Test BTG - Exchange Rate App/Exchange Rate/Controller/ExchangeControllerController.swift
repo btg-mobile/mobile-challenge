c4cc635ae9f9b790282101c474e19f112fa27699
= <marchini.developer@gmail.com>
2022-02-25 15:47:27 -0300

    Create ExchangeRateController and set it as initial Controller.

---
+++ Test BTG - Exchange Rate App/Test BTG - Exchange Rate App/Exchange Rate/Controller/ExchangeControllerController.swift
@@ -0,0 +1,19 @@
+//
+//  ExchangeControllerController.swift
+//  Test BTG - Exchange Rate App
+// 
+//  Created by Renan Marchini Andrusiac on 25/02/22
+//  Copyright © 2017 Renan Marchini Andrusiac. All rights reserved.
+//	
+
+import UIKit
+
+class ExchangeControllerController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        view.backgroundColor = .systemIndigo
+    }
+
+}
--- Test BTG - Exchange Rate App/Test BTG - Exchange Rate App/SceneDelegate.swift
+++ Test BTG - Exchange Rate App/Test BTG - Exchange Rate App/SceneDelegate.swift
@@ -14,10 +14,22 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 
 
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
-        // Use this method to optionally configure and attach the UIWindow `window` to the provided UIWindowScene `scene`.
-        // If using a storyboard, the `window` property will automatically be initialized and attached to the scene.
-        // This delegate does not imply the connecting scene or session are new (see `application:configurationForConnectingSceneSession` instead).
-        guard let _ = (scene as? UIWindowScene) else { return }
+        /// 1. Capture the scene
+        guard let windowScene = (scene as? UIWindowScene) else { return }
+        
+        /// 2. Create a new UIWindow using the windowScene constructor which takes in a window scene.
+        let window = UIWindow(windowScene: windowScene)
+        
+        /// 3. Create a view hierarchy programmatically
+        let viewController = ExchangeControllerController()
+        let navigation = UINavigationController(rootViewController: viewController)
+        
+        /// 4. Set the root view controller of the window with your view controller
+        window.rootViewController = navigation
+        
+        /// 5. Set the window and call makeKeyAndVisible()
+        self.window = window
+        window.makeKeyAndVisible()
     }
 
     func sceneDidDisconnect(_ scene: UIScene) {
