f76767e2b1409885847870a13ffc019f86052e75
joaoabreuact <93674925+joaoabreuact@users.noreply.github.com>
2022-07-18 11:34:59 -0300

    fixing some issues

--- CurrencyConverter/CurrencyConverterTests/Scenes/Currencies/CurrenciesUITests.swift
+++ CurrencyConverter/CurrencyConverterTests/Scenes/Currencies/CurrenciesUITests.swift
@@ -11,36 +11,37 @@ import XCTest
 import SnapshotTesting
 
 final class CurrenciesUITests: XCTestCase {
-    
+
     func testCurrenciesListSuccess() {
-        // givene
+        // given
         let serviceMock = CurrenciesServiceMock()
         let viewModel = CurrenciesViewModel(service: serviceMock)
         let mockedCurrenciesList = Currencies(currencies: ["AED": "United Arab Emirates Dirham"])
-        
+
         let sut = CurrenciesViewController(viewModel: viewModel)
         let navController = UINavigationController(rootViewController: sut)
+        
         // when
         serviceMock.result = .success(mockedCurrenciesList)
         sut.loadView()
-        
+
         // then
         assertSnapshot(matching: navController, as: .image(size: .init(width: 390, height: 844)))
 
     }
-    
+
     func testCurrenciesListdidFailToFetch() {
         let serviceMock = CurrenciesServiceMock()
         let viewModel = CurrenciesViewModel(service: serviceMock)
-        
+
         let sut = CurrenciesViewController(viewModel: viewModel)
         let navController = UINavigationController(rootViewController: sut)
         // when
         serviceMock.result = .failure(.parseError)
         sut.loadView()
-        
+
         // then
         assertSnapshot(matching: navController, as: .image(size: .init(width: 390, height: 844)))
     }
-    
+
 }
