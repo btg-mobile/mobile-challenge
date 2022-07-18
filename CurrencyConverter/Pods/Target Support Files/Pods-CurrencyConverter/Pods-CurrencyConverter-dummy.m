53473c63f43fa4a44de57ebd65f75efeb775de81
joaoabreuact <93674925+joaoabreuact@users.noreply.github.com>
2022-07-18 11:36:42 -0300

    cleaing repo

--- CurrencyConverter/Pods/Target Support Files/Pods-CurrencyConverter/Pods-CurrencyConverter-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_CurrencyConverter : NSObject
-@end
-@implementation PodsDummy_Pods_CurrencyConverter
-@end
--- CurrencyConverter/Pods/Target Support Files/Pods-CurrencyConverterTests/Pods-CurrencyConverterTests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_CurrencyConverterTests : NSObject
-@end
-@implementation PodsDummy_Pods_CurrencyConverterTests
-@end
