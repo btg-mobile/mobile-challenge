3bcf521e693163b496d133c9af3a84795cda274b
Vinicius Pansan <vinicius.spansan@gmail.com>
2025-02-01 18:43:14 -0300

    Add SearchBar

--- mobile-challenge/Mobile Challenge/ViewController/CurrencyListViewController.swift
+++ mobile-challenge/Mobile Challenge/ViewController/CurrencyListViewController.swift
@@ -7,15 +7,18 @@
 
 import UIKit
 
-class CurrencyListViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+class CurrencyListViewController: UIViewController, UITableViewDataSource, UITableViewDelegate, UISearchBarDelegate {
     
     let tableView = UITableView()
+    let searchBar = UISearchBar()
     var safeArea: UILayoutGuide!
     let currencyVM: CurrencyViewModel = CurrencyViewModel()
     let conversionVM: ConversionViewModel = ConversionViewModel()
     var onCurrencySelected: ((String, Float) -> Void)?
     
     var isPrimarySelection: Bool = false
+    var filteredCurrencies: [CurrencyNameModel] = []
+    var isSearching = false
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -38,14 +41,23 @@ class CurrencyListViewController: UIViewController, UITableViewDataSource, UITab
         
     }
     
+    
+    func setupSearchBar() {
+        searchBar.placeholder = "Buscar moeda"
+        searchBar.delegate = self
+        navigationItem.titleView = searchBar
+    }
+    
     //MARK: UI Configuration Function
     func setupUI() {
         setupTableView()
+        setupSearchBar()
         view.backgroundColor = .white
         safeArea = view.layoutMarginsGuide
         
         currencyVM.pullCurrencyNames { [weak self] in
             DispatchQueue.main.async {
+                self?.filteredCurrencies = self?.currencyVM.currencyNames ?? []
                 self?.tableView.reloadData()
             }
             
@@ -55,23 +67,44 @@ class CurrencyListViewController: UIViewController, UITableViewDataSource, UITab
     
     //MARK: TableViewDelegate
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return currencyVM.currencyNames.count
+        return filteredCurrencies.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        
         let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
-        let currency = currencyVM.currencyNames[indexPath.row]
+        let currency = filteredCurrencies[indexPath.row]
         cell.textLabel?.text = "\(currency.code) - \(currency.name)"
-        
         return cell
     }
     
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-        let selectedCurrencyCode = conversionVM.convertions[indexPath.row].code
-        let selectedCurrencyValue = conversionVM.convertions[indexPath.row].dolarValue
-        onCurrencySelected?(selectedCurrencyCode, selectedCurrencyValue)
-        navigationController?.popViewController(animated: true)
+        let selectedCurrency = filteredCurrencies[indexPath.row]
+        
+        if let conversion = conversionVM.convertions.first(where: { $0.code == selectedCurrency.code }) {
+            onCurrencySelected?(conversion.code, conversion.dolarValue)
+            navigationController?.popViewController(animated: true)
+        }
+    }
+    
+    //MARK: SearchBar Delegate
+    func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String) {
+        if searchText.isEmpty {
+            filteredCurrencies = currencyVM.currencyNames
+        } else {
+            filteredCurrencies = currencyVM.currencyNames.filter { currency in
+                currency.code.lowercased().contains(searchText.lowercased()) ||
+                currency.name.lowercased().contains(searchText.lowercased())
+            }
+        }
+        tableView.reloadData()
+    }
+    
+    func searchBarCancelButtonClicked(_ searchBar: UISearchBar) {
+        searchBar.text = ""
+        searchBar.resignFirstResponder()
+        filteredCurrencies = currencyVM.currencyNames
+        tableView.reloadData()
     }
     
 }
+
