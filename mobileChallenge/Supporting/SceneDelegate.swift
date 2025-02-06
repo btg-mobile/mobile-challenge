5161aef21283169f70722026ddc8df3befe8c438
henhiga <henrihiga@gmail.com>
2025-02-05 23:03:55 -0300

    added nav controller

--- mobileChallenge/Supporting/SceneDelegate.swift
+++ mobileChallenge/Supporting/SceneDelegate.swift
@@ -18,7 +18,11 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         // This delegate does not imply the connecting scene or session are new (see `application:configurationForConnectingSceneSession` instead).
         guard let windowScene = (scene as? UIWindowScene) else { return }
         let window = UIWindow(windowScene: windowScene)
-        window.rootViewController = HomeController()
+        
+        let vc = HomeController()
+        let nav = UINavigationController(rootViewController: vc)
+        
+        window.rootViewController = nav
         self.window = window
         self.window?.makeKeyAndVisible()
     }
