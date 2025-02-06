a9344de8c0667b671ca551bd64b32fa4b1cb6c54
henhiga <henrihiga@gmail.com>
2025-02-05 23:00:33 -0300

    added supporting

--- mobileChallenge/Supporting/SceneDelegate.swift
+++ mobileChallenge/Supporting/SceneDelegate.swift
@@ -18,11 +18,7 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         // This delegate does not imply the connecting scene or session are new (see `application:configurationForConnectingSceneSession` instead).
         guard let windowScene = (scene as? UIWindowScene) else { return }
         let window = UIWindow(windowScene: windowScene)
-        
-        let vc = HomeController()
-        let nav = UINavigationController(rootViewController: vc)
-        
-        window.rootViewController = nav
+        window.rootViewController = HomeController()
         self.window = window
         self.window?.makeKeyAndVisible()
     }
